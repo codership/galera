24997490d99f855f589fcc2ab84a59e0dcf1c745
Alexey Yurchenko <alexey.yurchenko@codership.com>
2008-04-02 15:14:58 +0000

    Updated package and library interface versions. This truly closes #37
    git-svn-id: https://devel.codership.com/svn/galera/trunk@253 9148e567-b586-4176-b9b8-4e36cb4ec61b

--- gcs/src/unit_tests/gcs_backend_test.c
+++ gcs/src/unit_tests/gcs_backend_test.c
@@ -10,16 +10,6 @@
 #include "../gcs_backend.h"
 #include "gcs_backend_test.h"
 
-enum
-{
-    DUMMY_BACKEND,
-    SPREAD_BACKEND,
-    VS_BACKEND,
-    ILL_BACKEND
-};
-
-static long opened = ILL_BACKEND;
-
 GCS_BACKEND_NAME_FN(gcs_dummy_name)
 {
     return "DUMMIEEEE!";
@@ -28,19 +18,28 @@ GCS_BACKEND_NAME_FN(gcs_dummy_name)
 GCS_BACKEND_OPEN_FN(gcs_dummy_open)
 {
     backend->name = gcs_dummy_name;
-    opened = DUMMY_BACKEND;
     return 0;
+}
+
+GCS_BACKEND_NAME_FN(gcs_spread_name)
+{
+    return "SPREAT";
 }
 
 GCS_BACKEND_OPEN_FN(gcs_spread_open)
 {
-    opened = SPREAD_BACKEND;
+    backend->name = gcs_spread_name;
     return 0;
+}
+
+GCS_BACKEND_NAME_FN(gcs_vs_name)
+{
+    return "vsssssssss";
 }
 
 GCS_BACKEND_OPEN_FN(gcs_vs_open)
 {
-    opened = VS_BACKEND;
+    backend->name = gcs_vs_name;
     return 0;
 }
 
@@ -49,28 +48,23 @@ START_TEST (gcs_backend_test)
     gcs_backend_t backend;
     long ret;
 
-    fail_if (opened != ILL_BACKEND);
-
     ret = gcs_backend_init (&backend, NULL, "wrong://kkk");
     fail_if (ret != -ESOCKTNOSUPPORT);
-    fail_if (opened != ILL_BACKEND);
 
     ret = gcs_backend_init (&backend, NULL, "spread:");
     fail_if (ret != -EINVAL);
-    fail_if (opened != ILL_BACKEND);
 
     ret = gcs_backend_init (&backend, NULL, "dummy://");
     fail_if (ret != 0, "ret = %d (%s)", ret, strerror(-ret));
     fail_if (backend.name != gcs_dummy_name);
-    fail_if (opened != DUMMY_BACKEND);
 
     ret = gcs_backend_init (&backend, NULL, "gcomm://kkk");
     fail_if (ret != 0, "ret = %d (%s)", ret, strerror(-ret));
-    fail_if (opened != VS_BACKEND);
+    fail_if (backend.name != gcs_vs_name);
 
     ret = gcs_backend_init (&backend, NULL, "spread://");
     fail_if (ret != 0, "ret = %d (%s)", ret, strerror(-ret));
-    fail_if (opened != SPREAD_BACKEND);
+    fail_if (backend.name != gcs_spread_name);
 
 }
 END_TEST
