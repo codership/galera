7a4f5fd6f5445b4b2a6fe95a0135a0eeca848868
Alexey Yurchenko <alexey.yurchenko@codership.com>
2008-04-03 17:08:37 +0000

    Added separate receiving message structure definition.
    git-svn-id: https://devel.codership.com/svn/galera/trunk@265 9148e567-b586-4176-b9b8-4e36cb4ec61b

--- gcs/src/gcs_recv_act.h
+++ gcs/src/gcs_recv_act.h
@@ -11,6 +11,12 @@
 #ifndef _gcs_recv_act_h_
 #define _gcs_recv_act_h_
 
+#include <string.h>   // for memset
+#include <stdbool.h>
+#include <galerautils.h>
+
+#include "gcs_recv_msg.h"
+
 typedef struct gcs_recv_act
 {
     gcs_seqno_t    send_no; // send id (unique for a node)
@@ -22,6 +28,38 @@ typedef struct gcs_recv_act
 }
 gcs_recv_act_t;
 
+/*!
+ * Handle received message - action fragment
+ *
+ * @return 0              - success,
+ *         size of action - success, full action received,
+ *         negative       - error.
+ */
+extern long
+gcs_recv_act_handle_msg (gcs_recv_act_t*       act,
+                         const gcs_recv_msg_t* msg,
+                         bool                  foreign);
+
+/*!
+ * Pop received action buffer and get ready to receive another
+ *
+ * @return pointer to action buffer, NULL if action is local - must be
+ *         fetched from local fifo.
+ */
+static inline uint8_t*
+core_pop_action (gcs_recv_act_t* act)
+{
+    register uint8_t* ret = act->head;
+
+    assert (act->size == act->received);
+
+    memset (act, 0, sizeof (*act));
+    act->send_no = GCS_SEQNO_ILL;
+
+    return ret;
+}
+
+
 /*! Deassociate, but don't deallocate action resources */
 static inline void
 gcs_recv_act_forget (gcs_recv_act_t* act)
@@ -29,7 +67,7 @@ gcs_recv_act_forget (gcs_recv_act_t* act)
     act->head = NULL;
 }
 
-/*! Free resources associated with the action */
+/*! Free resources associated with the action (for lost node cleanup) */
 static inline void
 gcs_recv_act_free (gcs_recv_act_t* act)
 {
---
+++ gcs/src/gcs_recv_msg.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2008 Codership Oy <info@codership.com>
+ *
+ * $Id$
+ */
+
+/*!
+ * Receiving message context
+ */
+
+#ifndef _gcs_recv_msg_h_
+#define _gcs_recv_msg_h_
+
+#include "gcs_msg_type.h"
+
+typedef struct gcs_recv_msg
+{
+    void*          buf;
+    long           buf_len;
+    long           size;
+    long           sender_id;    
+    gcs_msg_type_t type;
+}
+gcs_recv_msg_t;
+
+#endif /* _gcs_recv_msg_h_ */
