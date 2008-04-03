0381081c1b006af9a1bd6c9544c50f7d466eb171
Alexey Yurchenko <alexey.yurchenko@codership.com>
2008-04-03 11:13:20 +0000

    Got rid of obsolete build script, renamed new one - to avoid confusion.
    git-svn-id: https://devel.codership.com/svn/galera/trunk@262 9148e567-b586-4176-b9b8-4e36cb4ec61b

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# $Id$
+
 # Fail if any command fails
 set -e
 
@@ -18,8 +20,8 @@ have_ccache="false"
 #
 #fi
 
-initial_stage="scratch"
-last_stage="mysql"
+initial_stage="galerautils"
+last_stage="wsdb"
 gainroot=""
 
 usage()
@@ -27,9 +29,16 @@ usage()
     echo -e "Usage: build.sh [OPTIONS] \n" \
 	"Options:                      \n" \
         "    --stage <initial stage>   \n" \
-	"    --last-stage <last stage> \n"
+	"    --last-stage <last stage> \n" \
+	"    -s|--scratch    build everything from scratch\n"\
+	"    -c|--configure  reconfigure the build system (implies -s)\n"\
+	"    -b|--bootstap   rebuild the build system (implies -c)\n"\
+	"    -r|--release    configure build with debug disabled (implies -c)\n" \
+	"    -d|--debug      configure build with debug disabled (implies -c)\n"
 
 }
+
+set -x
 
 while test $# -gt 0 
 do
@@ -45,6 +54,21 @@ do
 	--gainroot)
 	    gainroot=$2
 	    shift
+	    ;;
+	-b|--bootstrap)
+	    BOOTSTRAP=yes # Bootstrap the build system
+	    ;;
+	-c|--configure)
+	    CONFIGURE=yes # Reconfigure the build system
+	    ;;
+	-s|--scratch)
+	    SCRATCH=yes   # Build from scratch (run make clean)
+	    ;;
+	-r|--release)
+	    RELEASE=yes   # Compile without debug
+	    ;;
+	-d|--debug)
+	    DEBUG=yes     # Compile with debug
 	    ;;
 	--help)
 	    usage
@@ -63,7 +87,6 @@ done
 set -x
 
 # Build process base directory
-#build_base=`pwd`
 build_base=$(cd $(dirname $0); cd ..; pwd -P)
 
 # Define branches to be used
@@ -79,46 +102,43 @@ if ! test -d $build_base/build ; then
 fi
 
 
-# Install destination setup
-prefix=${GALERA_BUILD_PREFIX:-"opt/galera"}
-destdir=${GALERA_BUILD_DEST:-"$build_base/build/dest"}
+# Flags for configure scripts
+if test -n GALERA_DEST
+then
+    conf_flags="--prefix=$GALERA_DEST"
+    galera_flags="--with-galera=$GALERA_DEST"
+fi
 
-export GALERA_DEST="$destdir/$prefix"
+if [ "$RELEASE" == "yes" ]
+then
+    conf_flags="$conf_flags --disable-debug"
+    CONFIGURE="yes"
+fi
+if [ "$DEBUG" == "yes" ]
+then
+    CONFIGURE="yes"
+fi
 
-
-#
-# export DESTDIR=$destdir
-# export LIBDIR="$destdir/lib"
-
-# Export this for configure scripts
-# For example wsdb conf calls AC_FUNC_MALLOC which tries
-# to link against -lgcs, which obviously is not found from
-# standard locations. 
-
-export LD_LIBRARY_PATH="$GALERA_DEST/lib"
-
-# Export build prefix also for mysql
-export MYSQL_BUILD_PREFIX=$GALERA_DEST
-
-# Flags for configure scripts 
-conf_flags="--prefix=$GALERA_DEST"
-galera_flags="--with-galera=$GALERA_DEST"
-
-
-# Cd to build directory and fetch sources
-cd $build_base/build
 
 # Function to build single project
 build()
 {
-    echo "Building: $1 ($@)"
-    cd $1
+    local build_dir=$1
     shift
-    ./bootstrap.sh
-    ./configure $@
-    make 
-    $gainroot make install
-    cd ..
+    echo "Building: $build_dir ($@)"
+    pushd $build_dir
+    export LD_LIBRARY_PATH
+    export CPPFLAGS
+    export LDFLAGS
+    if [ "$BOOTSTRAP" == "yes" ]; then ./bootstrap.sh; CONFIGURE=yes ; fi
+    if [ "$CONFIGURE" == "yes" ]; then rm -rf config.status; ./configure $@; SCRATCH=yes ; fi
+    if [ "$SCRATCH"   == "yes" ]; then make clean ; fi
+    make
+    LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$build_dir/src/.libs
+    CPPFLAGS="$CPPFLAGS -I$build_dir/src "
+    LDFLAGS="$LDFLAGS -L$build_dir/src/.libs"
+#    $gainroot make install
+    popd
 }
 
 building="false"
@@ -136,49 +156,31 @@ fi
 
 if test $initial_stage = "galerautils" || $building = "true"
 then
-    rm -rf galerautils
-    svn export --force $galerautils_branch galerautils
-    build galerautils $conf_flags
+    build $galerautils_branch $conf_flags
     building="true"
 fi
 
 if test $initial_stage = "galeracomm" || $building = "true"
 then
-    rm -rf galeracomm
-    svn export --force $galeracomm_branch galeracomm
-    build galeracomm $conf_flags $galera_flags
+    build $galeracomm_branch $conf_flags $galera_flags
+    CPPFLAGS="$CPPFLAGS -I$galeracomm_branch/vs/include" # non-standard location
+    CPPFLAGS="$CPPFLAGS -I$galeracomm_branch/common/include" # non-standard location
+    LDFLAGS="$LDFLAGS -L$galeracomm_branch/common/src/.libs"
+    LDFLAGS="$LDFLAGS -L$galeracomm_branch/transport/src/.libs"
+    LDFLAGS="$LDFLAGS -L$galeracomm_branch/vs/src/.libs"
     building="true"
 fi
 
 if test $initial_stage = "gcs" || $building = "true"
 then
-    rm -rf gcs
-    svn export --force $gcs_branch gcs
-    build gcs $conf_flags $galera_flags
+    build $gcs_branch $conf_flags $galera_flags
     building="true"
 fi
 
 if test $initial_stage = "wsdb" || $building = "true"
 then
-    rm -rf wsdb
-    svn export --force $wsdb_branch wsdb
-    build wsdb $conf_flags $galera_flags
-    building="true"
-fi
-
-if test $last_stage != "mysql"
-then
-    exit 0
-fi
-
-if test $initial_stage = "mysql" || $building = "true"
-then
-    rm -rf mysql
-    svn export --force $mysql_branch mysql
-    cd mysql
-    ./BUILD/compile-pentium-galera
-    $gainroot make install
-    cd ..
+    build $wsdb_branch $conf_flags $galera_flags
+    CPPFLAGS="$CPPFLAGS -I$wsdb_branch/include" # non-standard location
     building="true"
 fi
 
