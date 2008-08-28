205cb7cc049897176f1ae026b2a294c3c60b358c
Teemu Ollakka <teemu.ollakka@codership.com>
2008-08-28 13:14:28 +0000

    Moved HostList out of Driver
    git-svn-id: https://devel.codership.com/svn/galera/trunk@402 9148e567-b586-4176-b9b8-4e36cb4ec61b

--- java/drivers/galera-jdbc/com/codership/galera/jdbc/Driver.java
+++ java/drivers/galera-jdbc/com/codership/galera/jdbc/Driver.java
@@ -34,45 +34,6 @@ import java.util.Map;
 class Driver implements java.sql.Driver
 {
     java.sql.Driver realDriver = null;
-
-    class HostList
-    {
-        String[] hosts = null;
-        int      cur   = 0;
-        int[]    cnt   = null;
-
-        public HostList(String hstr)
-        {
-            hosts = hstr.split(",");
-            cnt = new int[hosts.length];
-            for (int i = 0; i < cnt.length; ++i)
-                cnt[i] = 0;
-        }
-
-        public String getNextHost()
-        {
-            if (hosts.length == 0)
-                return null;
-            String host = null;
-            synchronized (this)
-            {
-                host = hosts[cur];
-                cur = (cur + 1) % hosts.length;
-            }
-            return host;
-        }
-        public void increment(String host)
-        {
-            // TODO
-            // System.err.println("Hosts increment " + host);
-        }
-        public void decrement(String host)
-        {
-            // TODO
-            // System.err.println("Hosts decrement " + host);
-        }
-    }
-
     static HostList hosts = null;
 
     class Connection implements java.sql.Connection
