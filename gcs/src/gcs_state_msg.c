1cd6a30382343db9a8a83651a739e4a3f6000c76
Alexey Yurchenko <alexey.yurchenko@codership.com>
2008-09-17 12:37:00 +0000

    Added gcs_state_msg module.
    git-svn-id: https://devel.codership.com/svn/galera/trunk@425 9148e567-b586-4176-b9b8-4e36cb4ec61b

---
+++ gcs/src/gcs_state_msg.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2008 Codership Oy <info@codership.com>
+ *
+ * $Id$
+ */
+/*
+ * Interface to state messages - implementation
+ *
+ */
+
+//#define   GCS_STATE_MSG_ACCESS
+#include "gcs_state_msg.h"
+
---
+++ gcs/src/gcs_state_msg.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2008 Codership Oy <info@codership.com>
+ *
+ * $Id$
+ */
+/*
+ * Interface to state messages
+ *
+ */
+
+#ifndef _gcs_state_h_
+#define _gcs_state_h_
+
+
+#endif /* _gcs_state_h_ */
