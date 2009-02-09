d08db92dbb82afa616547f64f24f6151f558a87f
Alexey Yurchenko <alexey.yurchenko@codership.com>
2009-02-09 03:10:37 +0000

    Adding forgotten files
    git-svn-id: https://devel.codership.com/svn/galera/trunk@611 9148e567-b586-4176-b9b8-4e36cb4ec61b

---
+++ galera/src/galera_info.c
@@ -0,0 +1,58 @@
+// Copyright (C) 2009 Codership Oy <info@codership.com>
+
+#include <string.h>
+#include "galera_info.h"
+
+static size_t
+view_info_size (int members)
+{
+    return (sizeof(wsrep_view_info_t) + members * sizeof(wsrep_member_info_t));
+}
+
+/* create view info out of configuration message */
+wsrep_view_info_t* galera_view_info_create (const gcs_act_conf_t* conf)
+{
+    wsrep_view_info_t* ret = malloc(view_info_size(conf->memb_num));
+
+    if (ret) {
+        const char* str = conf->data;
+        int m;
+
+        ret->id       = *(wsrep_uuid_t*)&conf->group_uuid;
+        ret->conf     = conf->conf_id;
+        ret->first    = conf->seqno != GCS_SEQNO_ILL ?
+                        (conf->seqno + 1) : WSREP_SEQNO_UNDEFINED;
+        ret->status   = conf->conf_id != -1 ?
+                        WSREP_VIEW_PRIMARY : WSREP_VIEW_NON_PRIMARY;
+        ret->my_idx   = conf->my_idx;
+        ret->memb_num = conf->memb_num;
+
+        for (m = 0; m < ret->memb_num; m++) {
+            wsrep_member_info_t* member = &ret->members[m];
+            snprintf ((char*)&member->id,   sizeof(wsrep_uuid_t), "%s", str);
+            str = str + strlen (str) + 1;
+            snprintf (member->name, WSREP_MEMBER_NAME_LEN, "unknown");
+            member->ip_incoming     = 0;
+            member->ip_repl         = 0;
+            member->ip_sst          = 0;
+            member->status          = WSREP_MEMBER_EMPTY;
+            member->last_committed  = WSREP_SEQNO_UNDEFINED;
+            member->slave_queue_len = WSREP_SEQNO_UNDEFINED;
+            member->cpu_usage       = -1;
+            member->load_avg        = -1;
+        }
+    }
+
+    return ret;
+}
+
+/* make a copy of view info object */
+wsrep_view_info_t* galera_view_info_copy (const wsrep_view_info_t* vi)
+{
+    size_t ret_size = view_info_size (vi->memb_num);
+    wsrep_view_info_t* ret = malloc (ret_size);
+    if (ret) {
+        memcpy (ret, vi, ret_size);
+    }
+    return ret;
+}
