12a43f2a3ed0a0b661e7817731ac4b1423bf7d8a
Alexey Yurchenko <alexey.yurchenko@codership.com>
2009-06-02 20:26:52 +0000

    intermediate RPM-related commit: made rpmbuild to be run under mysql user
    git-svn-id: https://devel.codership.com/svn/galera/trunk@812 9148e567-b586-4176-b9b8-4e36cb4ec61b

--- scripts/mysql/rpm.sh
+++ scripts/mysql/rpm.sh
@@ -89,13 +89,13 @@ pushd libmysqld;  make link_sources > /dev/null; popd
 pushd client;     make link_sources > /dev/null; popd
 time make dist > /dev/null
 
-MYSQL_VER=$(grep '#define VERSION' $MYSQL_SRC/include/config.h | sed s/\"//g | cut -d ' ' -f 3 | cut -d '-' -f 1-2)
+MYSQL_VER=$(grep 'MYSQL_NO_DASH_VERSION' $MYSQL_SRC/Makefile | cut -d ' ' -f 3)
 
 #if test -d /usr/src/redhat
 #then
 #export RPM_BUILD_ROOT=/usr/src/redhat
 #else
-RPM_BUILD_ROOT=$BUILD_ROOT/redhat
+RPM_BUILD_ROOT=/tmp/redhat
 #fi
 mkdir -p $RPM_BUILD_ROOT
 pushd $RPM_BUILD_ROOT
@@ -107,6 +107,8 @@ popd; popd
 mv mysql-$MYSQL_VER.tar.gz $RPM_BUILD_ROOT/SOURCES/
 
 MYSQL_SPEC=$MYSQL_SRC/support-files/mysql-$MYSQL_VER.spec
+mv $MYSQL_SPEC $RPM_BUILD_ROOT/SPECS
+MYSQL_SPEC=$RPM_BUILD_ROOT/SPECS/mysql-$MYSQL_VER.spec
 
 i686_cflags="-march=i686 -mtune=i686"
 amd64_cflags="-m64 -mtune=opteron"
@@ -115,100 +117,15 @@ uname -m | grep -q i686 && \
 export RPM_OPT_FLAGS="$i686_cflags $fast_cflags"   || \
 export RPM_OPT_FLAGS="$amd64_cflags $fast_cflags"
 
-time rpmbuild \
-      --define "_topdir $RPM_BUILD_ROOT" \
-      --define "optflags $RPM_OPT_FLAGS" \
-      --with wsrep -ba $MYSQL_SPEC \
-      > $BUILD_ROOT/rpmbuild.log
+
+RPMBUILD="rpmbuild --clean --rmsource \
+          --define \"_topdir $RPM_BUILD_ROOT\" \
+          --define \"optflags $RPM_OPT_FLAGS\" \
+          --with wsrep -ba $MYSQL_SPEC \
+          > $RPM_BUILD_ROOT/rpmbuild.log"
+
+chown -R mysql $RPM_BUILD_ROOT
+su mysql -c "$RPMBUILD"
 
 exit 0
 
-# Build mysqld
-if [ "$CONFIGURE" == "yes" ]
-then
-    rm -f config.status
-    if [ "$DEBUG" == "yes" ]
-    then
-        BUILD/compile-$CPU-debug-wsrep
-    else
-        BUILD/compile-$CPU-wsrep
-    fi
-else # just recompile and relink with old configuration
-    make
-fi
-
-######################################
-##                                  ##
-##      Making of distribution      ##
-##                                  ##
-######################################
-
-# Create build directory structure
-DIST_DIR=$BUILD_ROOT/dist
-MYSQL_DIST_DIR=$DIST_DIR/mysql
-GALERA_DIST_DIR=$DIST_DIR/galera
-cd $BUILD_ROOT
-rm -rf $DIST_DIR
-
-# Install required MySQL files in the DIST_DIR
-mkdir -p $MYSQL_DIST_DIR
-install -m 644 LICENSE $DIST_DIR
-install -m 755 mysql-galera $DIST_DIR
-install -m 755 gcomm $DIST_DIR
-install -m 644 LICENSE.mysql $MYSQL_DIST_DIR
-install -m 644 README $DIST_DIR
-install -m 644 QUICK_START $DIST_DIR
-install -D -m 644 $MYSQL_SRC/sql/share/english/errmsg.sys $MYSQL_DIST_DIR/share/mysql/english/errmsg.sys
-install -D -m 755 $MYSQL_SRC/sql/mysqld $MYSQL_DIST_DIR/libexec/mysqld
-install -D -m 755 $MYSQL_SRC/scripts/wsrep_sst_mysqldump $MYSQL_DIST_DIR/bin/wsrep_sst_mysqldump
-install -D -m 644 my.cnf $MYSQL_DIST_DIR/etc/my.cnf
-tar -xzf mysql_var.tgz -C $MYSQL_DIST_DIR
-
-# Copy required Galera libraries
-GALERA_LIBS=$GALERA_DIST_DIR/lib
-mkdir -p $GALERA_LIBS
-install -m 644 LICENSE.galera $GALERA_DIST_DIR
-cp -P $GALERA_SRC/galerautils/src/.libs/libgalerautils.so* $GALERA_LIBS
-if test $GCOMM_IMPL = "galeracomm"
-then
-    cp -P $GALERA_SRC/galeracomm/common/src/.libs/libgcommcommonpp.so* $GALERA_LIBS
-    cp -P $GALERA_SRC/galeracomm/transport/src/.libs/libgcommtransportpp.so* $GALERA_LIBS
-    cp -P $GALERA_SRC/galeracomm/vs/src/.libs/libgcommvspp.so* $GALERA_LIBS
-else
-    cp -P $GALERA_SRC/gcomm/src/.libs/libgcomm.so* $GALERA_LIBS
-fi
-cp -P $GALERA_SRC/gcs/src/.libs/libgcs.so* $GALERA_LIBS
-cp -P $GALERA_SRC/wsdb/src/.libs/libwsdb.so* $GALERA_LIBS
-cp -P $GALERA_SRC/galera/src/.libs/libmmgalera.so* $GALERA_LIBS
-
-GALERA_SBIN=""
-if test $GCOMM_IMPL = "galeracomm"
-    then
-# Install vs backend daemon
-    GALERA_SBIN=$GALERA_DIST_DIR/sbin
-    mkdir -p $GALERA_SBIN
-    install -D -m 755 $GALERA_SRC/galeracomm/vs/src/.libs/vsbes $GALERA_SBIN/vsbes
-fi
-
-# Strip binaries if not instructed otherwise
-if test "$NO_STRIP" != "yes"
-then
-    strip $GALERA_LIBS/lib*.so
-    if test $GCOMM_IMPL = "galeracomm"
-	then
-	strip $GALERA_SBIN/*
-    fi
-fi
-
-# original MYSQL_VER=$(grep AM_INIT_AUTOMAKE\(mysql, configure.in | awk '{ print $2 }' | sed s/\)//)
-MYSQL_VER=$(grep '#define VERSION' $MYSQL_SRC/include/config.h | sed s/\"//g | cut -d ' ' -f 3 | cut -d '-' -f 1-2)
-RELEASE_NAME=$(echo mysql-$MYSQL_VER-$MYSQL_REV,$GALERA_REV | sed s/\:/_/g)
-rm -rf $RELEASE_NAME
-mv $DIST_DIR $RELEASE_NAME
-
-# Pack the release
-if [ "$TAR" == "yes" ]
-then
-    tar -czf $RELEASE_NAME.tgz $RELEASE_NAME
-fi
-#
