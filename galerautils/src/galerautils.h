03ce6809b7a4a81165c9838cd4c102b0bed3693e
Alexey Yurchenko <alexey.yurchenko@codership.com>
2009-06-07 18:07:15 +0000

    Intermediate commit in preparation for galerautils++
    git-svn-id: https://devel.codership.com/svn/galera/trunk@819 9148e567-b586-4176-b9b8-4e36cb4ec61b

--- galerautils/src/galerautils.h
+++ galerautils/src/galerautils.h
@@ -9,35 +9,7 @@
 #ifndef _galerautils_h_
 #define _galerautils_h_
 
-/* "Shamelessly stolen" (tm) goods from Linux kernel */
-/*
- * min()/max() macros that also do
- * strict type-checking.. See the
- * "unnecessary" pointer comparison.
- */
-
-#define GU_MAX(x,y) ({       \
-        typeof(x) _x = (x);  \
-        typeof(y) _y = (y);  \
-        (void) (&_x == &_y); \
-        _x > _y ? _x : _y; })
-
-#define GU_MIN(x,y) ({       \
-        typeof(x) _x = (x);  \
-        typeof(y) _y = (y);  \
-        (void) (&_x == &_y); \
-        _x < _y ? _x : _y; })
-
-#define gu_offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
-
-#if __GNUC__ >= 3
-#define gu_likely(x)   __builtin_expect((x), 1)
-#define gu_unlikely(x) __builtin_expect((x), 0)
-#else
-#define gu_likely(x)   (x)
-#define gu_unlikely(x) (x)
-#endif
-
+#include "gu_macros.h"
 #include "gu_limits.h"
 #include "gu_log.h"
 #include "gu_assert.h"
