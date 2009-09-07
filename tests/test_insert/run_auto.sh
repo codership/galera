c7f610e0bddb55f3983091726ad4c64278f68856
Alexey Yurchenko <alexey.yurchenko@codership.com>
2009-09-07 10:01:29 +0000

    - made insert test more CPU bound - fixed DBMS_PORT fallback in cluster.conf.tmpl
    git-svn-id: https://devel.codership.com/svn/galera/trunk@1027 9148e567-b586-4176-b9b8-4e36cb4ec61b

--- tests/test_insert/run_auto.sh
+++ tests/test_insert/run_auto.sh
@@ -1,12 +1,9 @@
-#!/bin/sh
-
-set -e
-set -u
+#!/bin/sh -eu
 
 TEST_BASE=$(cd $(dirname $0)/..; pwd -P)
 . $TEST_BASE/conf/main.conf
 
-TRX_LEN=10
+TRX_LEN=1
 
 # Auto gernerate does not seem to work with mysqlslap 5.1.37
 #	  --number-int-cols=5 --number-char-cols=3 \
@@ -16,12 +13,13 @@ TRX_LEN=10
 #	  --auto-generate-sql-unique-write-number=10000 \
 #	  --auto-generate-sql-write-number=$TRX_LEN \
 #	  --auto-generate-sql-load-type=write \
+#         --detach=$(($TRX_LEN * 4)) # detach is broken, m'kay?
 mysqlslap --user=$DBMS_TEST_USER --password=$DBMS_TEST_PSWD \
           --host=$DBMS_HOST --port=$DBMS_PORT \
           --delimiter=';' --create-schema=$DBMS_TEST_SCHEMA \
 	  --create="create_auto.sql" --query="insert_auto.sql" \
-          --commit=$TRX_LEN --detach=$(($TRX_LEN * 4)) \
-	  --concurrency=30 --number-of-queries=10000 \
+          --commit=$TRX_LEN \
+	  --concurrency=$DBMS_CLIENTS --number-of-queries=1000000 \
 	  --verbose --csv
 
 #
