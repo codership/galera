902fefeb0d514405d04ddaef5c9c3bd6f42b5a85
Teemu Ollakka <teemu.ollakka@codership.com>
2009-11-20 16:52:01 +0000

    refs #151
    Got rid of unnecessary indentation.
    
    
    
    git-svn-id: https://devel.codership.com/svn/galera/trunk@1309 9148e567-b586-4176-b9b8-4e36cb4ec61b

--- galerautils/src/gu_resolver.hpp
+++ galerautils/src/gu_resolver.hpp
@@ -23,9 +23,8 @@
 // Forward declarations
 namespace gu
 {
-    
     class URI;
-}
+} // namespace gu
 
 // Declarations
 namespace gu
@@ -33,189 +32,24 @@ namespace gu
     namespace net
     {
         /*!
+         * @class Sockaddr 
+         *
+         * @brief Class encapsulating struct sockaddr.
+         *
          * Class encapsulating struct sockaddr and providing 
          * simple interface to access sockaddr fields.
          */ 
-        class Sockaddr
-        {
-        public:
-            /*!
-             * Default constuctor.
-             *
-             * @param sa     Pointer to sockaddr struct
-             * @param sa_len Length of sockaddr struct
-             */
-            Sockaddr(const sockaddr* sa, socklen_t sa_len);
-            
-            /*!
-             * Copy constructor.
-             *
-             * @param sa Reference to Sockaddr
-             */
-            Sockaddr(const Sockaddr& sa);
-            
-            /*!
-             * Destructor
-             */
-            ~Sockaddr();
-            
-            /*!
-             * Get address family.
-             *
-             * @return Address family
-             */
-            sa_family_t get_family() const { return sa_->sa_family; }
-            
-            /*!
-             * Get port in network byte order. This is applicable only
-             * for AF_INET, AF_INET6.
-             *
-             * @return Port in nework byte order
-             */
-            short get_port() const
-            {
-                switch(sa_->sa_family)
-                {
-                case AF_INET:
-                    return reinterpret_cast<const sockaddr_in*>(sa_)->sin_port;
-                case AF_INET6:
-                    return reinterpret_cast<const sockaddr_in6*>(sa_)->sin6_port;
-                default:
-                    gu_throw_fatal; throw;
-                }
-            }
-            
-            /*!
-             * Get pointer to address. Return value is pointer to void,
-             * user must do casting by himself.
-             *
-             * @todo: Figure out how this could be done in type safe way.
-             *
-             * @return Void pointer to address element.
-             */
-            const void* get_addr() const
-            {
-                switch(sa_->sa_family)
-                {
-                case AF_INET:
-                    return &reinterpret_cast<const sockaddr_in*>(sa_)->sin_addr;
-                case AF_INET6:
-                    return &reinterpret_cast<const sockaddr_in6*>(sa_)->sin6_addr;
-                default:
-                    gu_throw_fatal; throw;
-                }                
-            }
-            
-            /*!
-             * Get non-const reference to sockaddr struct. 
-             *
-             * @return Non-const reference to sockaddr struct.
-             */
-            sockaddr& get_sockaddr() { return *sa_; }
-
-            /*!
-             * Get const reference to sockaddr struct.
-             *
-             * @return Const reference to sockaddr struct.
-             */
-            const sockaddr& get_sockaddr() const { return *sa_; }
-
-            /*!
-             * Get length of sockaddr struct.
-             *
-             * @return Length of sockaddr struct
-             */
-            socklen_t get_sockaddr_len() const { return sa_len_; }
-            
-        public:
-            void operator=(const Sockaddr&);
-
-            sockaddr* sa_;
-            socklen_t sa_len_;
-        };
-
+        class Sockaddr;
         
         /*!
+         * @class Addrinfo 
+         *
+         * @brief Class encapsulating struct addrinfo.
+         *
          * Class encapsulating struct addrinfo and providing interface
          * to access addrinfo fields.
          */
-        class Addrinfo
-        {
-        public:
-            /*!
-             * Default constructor.
-             *
-             * @param ai Const reference to addrinfo struct
-             */
-            Addrinfo(const addrinfo& ai);
-
-            /*!
-             * Copy costructor.
-             * 
-             * @param ai Const reference to Addrinfo object to copy
-             */
-            Addrinfo(const Addrinfo& ai);
-
-            /*!
-             * Copy constructor that replaces @ai sockaddr struct.
-             *
-             * @param ai Const reference to Addrinfo object to copy
-             * @param sa Const reference to Sockaddr struct that replaces
-             *           @ai sockaddr data
-             */
-            Addrinfo(const Addrinfo& ai, const Sockaddr& sa);
-
-            /*!
-             * Destructor.
-             */
-            ~Addrinfo();
-
-            /*!
-             * Get address family, AF_INET, AF_INET6 etc.
-             *
-             * @return Address family
-             */
-            int get_family() const { return ai_.ai_family; }
-
-            /*!
-             * Get socket type, SOCK_STREAM, SOCK_DGRAM etc
-             *
-             * @return Socket type
-             */
-            int get_socktype() const { return ai_.ai_socktype; }
-
-            /*!
-             * Get protocol.
-             *
-             * @return Protocol
-             */
-            int get_protocol() const { return ai_.ai_protocol; }
-            
-            /*!
-             * Get length of associated sockaddr struct
-             *
-             * @return Length of associated sockaddr struct
-             */
-            socklen_t get_addrlen() const { return ai_.ai_addrlen; }
-
-            /*!
-             * Get associated Sockaddr object.
-             *
-             * @return Associated Sockaddr object
-             */
-
-            Sockaddr get_addr() const 
-            { return Sockaddr(ai_.ai_addr, ai_.ai_addrlen); }
-
-            /*!
-             * Get string representation of the addrinfo.
-             *
-             * @return String representation of the addrinfo
-             */
-            std::string to_string() const;
-        private:
-            addrinfo ai_;
-        };
+        class Addrinfo;
         
         /*!
          * Resolve address given in @uri
@@ -227,5 +61,184 @@ namespace gu
         Addrinfo resolve(const gu::URI& uri);
     } // namespace net
 } // namespace gu
+
+
+class gu::net::Sockaddr
+{
+public:
+    /*!
+     * Default constuctor.
+     *
+     * @param sa     Pointer to sockaddr struct
+     * @param sa_len Length of sockaddr struct
+     */
+    Sockaddr(const sockaddr* sa, socklen_t sa_len);
+            
+    /*!
+     * Copy constructor.
+     *
+     * @param sa Reference to Sockaddr
+     */
+    Sockaddr(const Sockaddr& sa);
+            
+    /*!
+     * Destructor
+     */
+    ~Sockaddr();
+            
+    /*!
+     * Get address family.
+     *
+     * @return Address family
+     */
+    sa_family_t get_family() const { return sa_->sa_family; }
+            
+    /*!
+     * Get port in network byte order. This is applicable only
+     * for AF_INET, AF_INET6.
+     *
+     * @return Port in nework byte order
+     */
+    short get_port() const
+    {
+        switch(sa_->sa_family)
+        {
+        case AF_INET:
+            return reinterpret_cast<const sockaddr_in*>(sa_)->sin_port;
+        case AF_INET6:
+            return reinterpret_cast<const sockaddr_in6*>(sa_)->sin6_port;
+        default:
+            gu_throw_fatal; throw;
+        }
+    }
+            
+    /*!
+     * Get pointer to address. Return value is pointer to void,
+     * user must do casting by himself.
+     *
+     * @todo: Figure out how this could be done in type safe way.
+     *
+     * @return Void pointer to address element.
+     */
+    const void* get_addr() const
+    {
+        switch(sa_->sa_family)
+        {
+        case AF_INET:
+            return &reinterpret_cast<const sockaddr_in*>(sa_)->sin_addr;
+        case AF_INET6:
+            return &reinterpret_cast<const sockaddr_in6*>(sa_)->sin6_addr;
+        default:
+            gu_throw_fatal; throw;
+        }                
+    }
+            
+    /*!
+     * Get non-const reference to sockaddr struct. 
+     *
+     * @return Non-const reference to sockaddr struct.
+     */
+    sockaddr& get_sockaddr() { return *sa_; }
+
+    /*!
+     * Get const reference to sockaddr struct.
+     *
+     * @return Const reference to sockaddr struct.
+     */
+    const sockaddr& get_sockaddr() const { return *sa_; }
+
+    /*!
+     * Get length of sockaddr struct.
+     *
+     * @return Length of sockaddr struct
+     */
+    socklen_t get_sockaddr_len() const { return sa_len_; }
+            
+public:
+    void operator=(const Sockaddr&);
+
+    sockaddr* sa_;
+    socklen_t sa_len_;
+};
+
+        
+class gu::net::Addrinfo
+{
+public:
+    /*!
+     * Default constructor.
+     *
+     * @param ai Const reference to addrinfo struct
+     */
+    Addrinfo(const addrinfo& ai);
+
+    /*!
+     * Copy costructor.
+     * 
+     * @param ai Const reference to Addrinfo object to copy
+     */
+    Addrinfo(const Addrinfo& ai);
+
+    /*!
+     * Copy constructor that replaces @ai sockaddr struct.
+     *
+     * @param ai Const reference to Addrinfo object to copy
+     * @param sa Const reference to Sockaddr struct that replaces
+     *           @ai sockaddr data
+     */
+    Addrinfo(const Addrinfo& ai, const Sockaddr& sa);
+
+    /*!
+     * Destructor.
+     */
+    ~Addrinfo();
+
+    /*!
+     * Get address family, AF_INET, AF_INET6 etc.
+     *
+     * @return Address family
+     */
+    int get_family() const { return ai_.ai_family; }
+
+    /*!
+     * Get socket type, SOCK_STREAM, SOCK_DGRAM etc
+     *
+     * @return Socket type
+     */
+    int get_socktype() const { return ai_.ai_socktype; }
+
+    /*!
+     * Get protocol.
+     *
+     * @return Protocol
+     */
+    int get_protocol() const { return ai_.ai_protocol; }
+            
+    /*!
+     * Get length of associated sockaddr struct
+     *
+     * @return Length of associated sockaddr struct
+     */
+    socklen_t get_addrlen() const { return ai_.ai_addrlen; }
+
+    /*!
+     * Get associated Sockaddr object.
+     *
+     * @return Associated Sockaddr object
+     */
+
+    Sockaddr get_addr() const 
+    { return Sockaddr(ai_.ai_addr, ai_.ai_addrlen); }
+
+    /*!
+     * Get string representation of the addrinfo.
+     *
+     * @return String representation of the addrinfo
+     */
+    std::string to_string() const;
+private:
+    addrinfo ai_;
+};
+        
 
 #endif /* __GU_RESOLVER_HPP__ */
