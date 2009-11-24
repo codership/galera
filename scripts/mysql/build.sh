0b2e6fa72d60acd0619c3b37c612b65c2c06dc53
Alexey Yurchenko <alexey.yurchenko@codership.com>
2009-11-24 22:17:49 +0000

    Fixed packaging to skip vsbes if not available. Fixes #224
    git-svn-id: https://devel.codership.com/svn/galera/trunk@1334 9148e567-b586-4176-b9b8-4e36cb4ec61b

--- scripts/mysql/build.sh
+++ scripts/mysql/build.sh
@@ -173,13 +173,11 @@ MYSQL_PLUGINS=$MYSQL_DIST_DIR/lib/mysql/plugin
 mkdir -p $MYSQL_PLUGINS
 install -m 644 LICENSE $DIST_DIR
 install -m 755 mysql-galera $DIST_DIR
-install -m 755 vsbes $DIST_DIR
 install -m 644 LICENSE.mysql $MYSQL_DIST_DIR
 install -m 644 README $DIST_DIR
 install -m 644 QUICK_START $DIST_DIR
 install -D -m 644 $MYSQL_SRC/sql/share/english/errmsg.sys $MYSQL_DIST_DIR/share/mysql/english/errmsg.sys
 install -D -m 755 $MYSQL_SRC/sql/mysqld $MYSQL_DIST_DIR/libexec/mysqld
-#install -D -m 755 $MYSQL_SRC/libmysql/libmysqlclient.la $MYSQL_LIBS
 install -D -m 755 $MYSQL_SRC/libmysql/.libs/libmysqlclient.so $MYSQL_LIBS
 install -D -m 755 $MYSQL_SRC/storage/innodb_plugin/.libs/ha_innodb_plugin.so $MYSQL_PLUGINS
 install -D -m 755 $MYSQL_SRC/client/.libs/mysql       $MYSQL_DIST_DIR/bin/mysql
@@ -196,26 +194,20 @@ mkdir -p $GALERA_LIBS
 install -m 644 LICENSE.galera $GALERA_DIST_DIR
 cp -P $GALERA_SRC/galerautils/src/.libs/libgalerautils.so*   $GALERA_LIBS
 cp -P $GALERA_SRC/galerautils/src/.libs/libgalerautils++.so* $GALERA_LIBS
-#if test $GCOMM_IMPL = "galeracomm"
-#then
-    cp -P $GALERA_SRC/galeracomm/common/src/.libs/libgcommcommonpp.so* $GALERA_LIBS
-    cp -P $GALERA_SRC/galeracomm/transport/src/.libs/libgcommtransportpp.so* $GALERA_LIBS
-    cp -P $GALERA_SRC/galeracomm/vs/src/.libs/libgcommvspp.so* $GALERA_LIBS
-#else
-    cp -P $GALERA_SRC/gcomm/src/.libs/libgcomm.so* $GALERA_LIBS
-#fi
+cp -P $GALERA_SRC/gcomm/src/.libs/libgcomm.so* $GALERA_LIBS
 cp -P $GALERA_SRC/gcs/src/.libs/libgcs.so* $GALERA_LIBS
 cp -P $GALERA_SRC/wsdb/src/.libs/libwsdb.so* $GALERA_LIBS
 cp -P $GALERA_SRC/galera/src/.libs/libmmgalera.so* $GALERA_LIBS
 
+# Install vsbes stuff if it is available
 GALERA_SBIN=""
-#if test $GCOMM_IMPL = "galeracomm"
-#    then
-# Install vs backend daemon
-    GALERA_SBIN=$GALERA_DIST_DIR/sbin
-    mkdir -p $GALERA_SBIN
-    install -D -m 755 $GALERA_SRC/galeracomm/vs/src/.libs/vsbes $GALERA_SBIN/vsbes
-#fi
+    cp -P $GALERA_SRC/galeracomm/common/src/.libs/libgcommcommonpp.so* $GALERA_LIBS && \
+    cp -P $GALERA_SRC/galeracomm/transport/src/.libs/libgcommtransportpp.so* $GALERA_LIBS && \
+    cp -P $GALERA_SRC/galeracomm/vs/src/.libs/libgcommvspp.so* $GALERA_LIBS && \
+    GALERA_SBIN=$GALERA_DIST_DIR/sbin && \
+    mkdir -p $GALERA_SBIN && \
+    install -D -m 755 $GALERA_SRC/galeracomm/vs/src/.libs/vsbes $GALERA_SBIN/vsbes && \
+    install -m 755 vsbes $DIST_DIR || echo "Skipping vsbes"
 
 strip $MYSQL_DIST_DIR/bin/mysql
 strip $MYSQL_DIST_DIR/bin/mysqladmin
