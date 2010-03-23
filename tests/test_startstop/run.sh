7ad86793fdbb695657bc5c36e67e62aed31c216b
Teemu Ollakka <teemu.ollakka@codership.com>
2010-03-23 14:14:11 +0000

    refs #295 Test which starts and stops cluster continuously
    git-svn-id: https://devel.codership.com/svn/galera/branches/0.7@1532 9148e567-b586-4176-b9b8-4e36cb4ec61b

---
+++ tests/test_startstop/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -eu
+
+declare -r DIST_BASE=$(cd $(dirname $0)/..; pwd -P)
+TEST_BASE=${TEST_BASE:-"$DIST_BASE"}
+
+. $TEST_BASE/conf/main.conf
+
+declare -r SCRIPTS="$DIST_BASE/scripts"
+. $SCRIPTS/jobs.sh
+. $SCRIPTS/action.sh
+. $SCRIPTS/signal.sh
+. $SCRIPTS/misc.sh
+
+TRIES=${1:-"-1"} # -1 stands for indefinite loop
+
+stop
+try=0
+
+while [ $try -ne $TRIES ]
+do
+    ! let try++ # ! - to protect from -e
+
+    echo -e "\nTry ${try}/${TRIES}\n"
+
+
+    start
+    pause 5
+    stop
+
+done
