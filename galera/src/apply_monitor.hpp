382136a6df41e682883d4e691f9750b758643b90
Alexey Yurchenko <alexey.yurchenko@codership.com>
2010-05-20 17:18:03 +0000

    Fixed apply_monitor to compile with GCC 4.1.2. Refs #296
    git-svn-id: https://devel.codership.com/svn/galera/trunk@1695 9148e567-b586-4176-b9b8-4e36cb4ec61b

--- galera/src/apply_monitor.hpp
+++ galera/src/apply_monitor.hpp
@@ -1,4 +1,4 @@
-
+/* Copyright (C) 2010 Codership Oy */
 #ifndef GALERA_APPLY_MONITOR_HPP
 #define GALERA_APPLY_MONITOR_HPP
 
@@ -13,11 +13,21 @@ namespace galera
     class Monitor
     {
     private:
+
         struct Applier
         {
             Applier() : trx_(0), cond_(), state_(S_IDLE) { }
+
+            Applier(const Applier& other)
+                :
+                trx_(other.trx_),
+                cond_(other.cond_),
+                state_(other.state_)
+            { }
+
             const TrxHandle* trx_;
             gu::Cond cond_;
+
             enum State
             {
                 S_IDLE,     // Slot is free
@@ -26,66 +36,65 @@ namespace galera
                 S_APPLYING, // Applying
                 S_FINISHED  // Finished
             } state_;
-            Applier(const Applier& other) 
-                : 
-                trx_(other.trx_), 
-                cond_(other.cond_),
-                state_(other.state_) { }
+
         private:
+
             void operator=(const Applier&);
         };
+
         static const size_t appliers_size_ = (1 << 14);
         static const size_t appliers_mask_ = appliers_size_ - 1;
+
     public:
+
         enum Mode
         {
             M_BYPASS,
             M_NORMAL
         };
 
-        Monitor() 
-            : 
+        Monitor()
+            :
             condition_(),
             mode_(M_NORMAL),
-            mutex_(), 
+            mutex_(),
             pre_enter_cond_(),
-            last_entered_(-1), 
+            last_entered_(-1),
             last_left_(-1),
             appliers_(appliers_size_),
             ooe_(0),
             ool_(0)
         { }
-        
-        ~Monitor()
-        {
-            log_info << "ooe " << ooe_ << " ool " << ool_;
-        }
-        
+
+        ~Monitor() { log_info << "ooe " << ooe_ << " ool " << ool_; }
+
         void assign_mode(Mode mode) { mode_ = mode; }
-        
-        void assign_initial_position(wsrep_seqno_t seqno) 
+
+        void assign_initial_position(wsrep_seqno_t seqno)
         {
             assert(last_entered_ == last_left_);
             log_info << "initial position " << seqno;
-            last_entered_ = last_left_ = seqno; 
+            last_entered_ = last_left_ = seqno;
         }
-        
+
         int enter(TrxHandle* trx)
         {
             if (mode_ == M_BYPASS) return 0;
-            size_t idx(indexof(trx->get_global_seqno()));
+
+            wsrep_seqno_t trx_seqno = trx->get_global_seqno();
+            size_t   idx(indexof(trx_seqno));
             gu::Lock lock(mutex_);
-            
+
             pre_enter(trx, lock, idx);
-            
+
             if (appliers_[idx].state_ ==  Applier::S_CANCELED)
             {
                 return -ECANCELED;
             }
-            
+
             appliers_[idx].state_ = Applier::S_WAITING;
             appliers_[idx].trx_   = trx;
-            
+
             while (may_enter(trx) == false)
             {
                 trx->unlock();
@@ -96,33 +105,35 @@ namespace galera
                     return -ECANCELED;
                 }
             }
-            
+
             appliers_[idx].state_ = Applier::S_APPLYING;
-            
-            if (last_left_ < trx->get_global_seqno())
-            {
-                ++ooe_;
-            }
+
+            ooe_ += (last_left_ < trx_seqno);
+
             return 0;
         }
-        
+
         void leave(const TrxHandle* trx)
         {
             if (mode_ == M_BYPASS) return;
-            size_t idx(indexof(trx->get_global_seqno()));
+
+            wsrep_seqno_t trx_seqno = trx->get_global_seqno();
+            size_t   idx(indexof(trx_seqno));
             gu::Lock lock(mutex_);
-            
+
             assert(appliers_[idx].state_ == Applier::S_APPLYING ||
                    appliers_[idx].state_ == Applier::S_CANCELED);
+
             appliers_[idx].state_ = Applier::S_FINISHED;
-            
+
             assert(appliers_[indexof(last_left_)].state_ == Applier::S_IDLE);
-            
+
             // Note: We need two scans here
             // 1) Update last left
             // 2) Check waiters that may enter due to updated last left
             const wsrep_seqno_t prev_last_left(last_left_);
-            size_t n_waiters(0);
+            size_t              n_waiters(0);
+
             for (wsrep_seqno_t i = last_left_ + 1; i <= last_entered_; ++i)
             {
                 Applier& a(appliers_[indexof(i)]);
@@ -131,7 +142,7 @@ namespace galera
                 case Applier::S_FINISHED:
                     if (last_left_ + 1 == i)
                     {
-                        a.state_ = Applier::S_IDLE;
+                        a.state_   = Applier::S_IDLE;
                         last_left_ = i;
                     }
                     break;
@@ -146,7 +157,7 @@ namespace galera
                     gu_throw_fatal << "invalid state " << a.state_;
                 }
             }
-            
+
             for (wsrep_seqno_t i = prev_last_left + 1; 
                  n_waiters > 0 && i <= last_entered_; ++i)
             {
@@ -161,36 +172,39 @@ namespace galera
                 }
             }
             appliers_[idx].trx_ = 0;
-            
+
             assert(n_waiters == 0);
-            assert((last_left_ >= trx->get_global_seqno() && 
+            assert((last_left_ >= trx_seqno && 
                     appliers_[idx].state_ == Applier::S_IDLE) ||
                    appliers_[idx].state_ == Applier::S_FINISHED);
             assert(last_left_ != last_entered_ ||
                    appliers_[indexof(last_left_)].state_ == Applier::S_IDLE);
-            if (last_left_ > trx->get_global_seqno())
-            {
-                ++ool_;
-            }
-            // log_info << "apply monitor leave " << trx->get_global_seqno();
+
+            ool_ += (last_left_ > trx_seqno);
+
+            // log_info << "apply monitor leave " << trx_seqno;
         }
-        
+
         void self_cancel(TrxHandle* trx)
         {
             if (mode_ == M_BYPASS) return;
-            size_t idx(indexof(trx->get_global_seqno()));
+
+            size_t   idx(indexof(trx->get_global_seqno()));
             gu::Lock lock(mutex_);
+
             pre_enter(trx, lock, idx);
             appliers_[idx].state_ = Applier::S_CANCELED;
         }
-        
-        
+
         void cancel(const TrxHandle* trx)
         {
             if (mode_ == M_BYPASS) return;
-            size_t idx(indexof(trx->get_global_seqno()));
+
+            size_t   idx(indexof(trx->get_global_seqno()));
             gu::Lock lock(mutex_);
+
             pre_enter(0, lock, idx);
+
             if (appliers_[idx].state_ <= Applier::S_WAITING)
             {
                 appliers_[idx].state_ = Applier::S_CANCELED;
@@ -201,8 +215,9 @@ namespace galera
                 log_info << "cancel, applier state " << appliers_[idx].state_;
             }
         }
-        
+
     private:
+
         size_t indexof(wsrep_seqno_t seqno)
         {
             return (seqno & appliers_mask_);
@@ -212,33 +227,32 @@ namespace galera
         {
             return condition_(last_entered_, last_left_, trx);
         }
-        
+
         // wait until it is possible to grab slot in monitor, 
         // update last entered
         void pre_enter(TrxHandle* trx, gu::Lock& lock, const int idx)
         {
             assert(last_left_ <= last_entered_);
-            while (last_entered_ - last_left_ >= appliers_size_)
+
+            while (last_entered_ >= last_left_ + appliers_size_)
             {
-                if (trx != 0)
-                {
-                    trx->unlock();
-                }
+                if (trx != 0) { trx->unlock(); }
+
                 lock.wait(pre_enter_cond_);
-                if (trx != 0)
-                {
-                    trx->lock();
-                }
+
+                if (trx != 0) { trx->lock(); }
             }
 
             if (trx != 0)
             {
                 assert(appliers_[idx].state_ == Applier::S_IDLE ||
                        appliers_[idx].state_ == Applier::S_CANCELED);
+
                 while (appliers_[indexof(last_entered_ + 1)].state_ 
                        > Applier::S_IDLE)
                 {
                     Applier& a(appliers_[indexof(last_entered_ + 1)]);
+
                     if (a.state_ == Applier::S_WAITING &&
                         may_enter(a.trx_) == true)
                     {
@@ -246,14 +260,18 @@ namespace galera
                     }
                     ++last_entered_;
                 }
-                if (last_entered_ + 1 == trx->get_global_seqno())
+
+                wsrep_seqno_t trx_seqno = trx->get_global_seqno();
+                if (last_entered_ + 1 == trx_seqno)
                 {
-                    last_entered_ = trx->get_global_seqno();
+                    last_entered_ = trx_seqno;
                 }
+
                 while (appliers_[indexof(last_entered_ + 1)].state_ 
                        > Applier::S_IDLE)
                 {
                     Applier& a(appliers_[indexof(last_entered_ + 1)]);
+
                     if (a.state_ == Applier::S_WAITING &&
                         may_enter(a.trx_) == true)
                     {
@@ -279,8 +297,5 @@ namespace galera
         size_t ool_;
     };
 }
-
-
-
 
 #endif // GALERA_APPLY_MONITOR_HPP
