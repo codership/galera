07b527f764ff307631ee0185063d88a53341caf1
Alexey Yurchenko <alexey.yurchenko@codership.com>
2010-08-16 12:41:29 +0000

    Refs #376 - more files for gcs parametrization
    git-svn-id: https://devel.codership.com/svn/galera/trunk@1864 9148e567-b586-4176-b9b8-4e36cb4ec61b

---
+++ gcs/src/gcs_params.c
@@ -0,0 +1,8 @@
+/*
+ * Copyright (C) 2010 Codership Oy <info@codership.com>
+ *
+ * $Id$
+ */
+
+#include "gcs_params.h"
+
---
+++ gcs/src/gcs_params.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2010 Codership Oy <info@codership.com>
+ *
+ * $Id$
+ */
+#ifndef _gcs_params_h_
+#define _gcs_params_h_
+
+#include "gu_config.h"
+
+#endif /* _gcs_params_h_ */
+
