08e29e4179efc7051c714538a0000eec6d2d0f28
Alexey Yurchenko <alexey.yurchenko@codership.com>
2010-08-20 13:53:25 +0000

    Refs #376 - customized string conversion functions for parsing parameter values.
    git-svn-id: https://devel.codership.com/svn/galera/trunk@1868 9148e567-b586-4176-b9b8-4e36cb4ec61b

--- galerautils/src/gu_utils.c
+++ galerautils/src/gu_utils.c
@@ -8,5 +8,90 @@
 
 #include "gu_utils.h"
 
-const int iso_c_requirement = 0;
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <limits.h>
+
+const char*
+gu_str2ll (const char* str, long long* ll)
+{
+    char* ret;
+    int   shift = 0;
+
+    long long llret = strtoll (str, &ret, 0);
+
+    switch (ret[0])
+    {
+    case 'g':
+    case 'G':
+        shift += 10;
+    case 'm':
+    case 'M':
+        shift += 10;
+    case 'k':
+    case 'K':
+        shift += 10;
+        ret++;
+
+        if (llret == ((llret << (shift + 1)) >> (shift + 1))) {
+            llret = llret << shift;
+        }
+        else { /* ERANGE */
+            if (llret > 0) llret = LLONG_MAX;
+            else llret = LLONG_MIN;
+        }
+    default:
+        *ll = llret;
+    }
+
+    return ret;
+}
+
+const char*
+gu_str2dbl (const char* str, double* dbl)
+{
+    char* ret;
+    *dbl = strtod (str, &ret);
+    return ret;
+}
+
+const char*
+gu_str2bool (const char* str, bool* b)
+{
+    size_t len = strlen(str);
+    int    res = -1; /* no conversion */
+
+    switch (len)
+    {
+    case 1:
+        if ('1' == str[0]) res = 1;
+        if ('0' == str[0]) res = 0;
+        break;
+    case 2:
+        if (!strcasecmp(str, "on")) res = 1;
+        break;
+    case 3:
+        if (!strcasecmp(str, "off")) res = 0;
+        break;
+    case 4:
+        if (!strcasecmp(str, "true")) res = 1;
+        break;
+    case 5:
+        if (!strcasecmp(str, "false")) res = 0;
+        break;
+    }
+
+    *b = (res > 0);
+
+    return (res >= 0) ? (str + len) : str;
+}
+
+const char*
+gu_str2ptr (const char* str, void** ptr)
+{
+    char* ret;
+    *ptr = (void*) strtoll (str, &ret, 16);
+    return ret;
+}
 
--- galerautils/src/gu_utils.h
+++ galerautils/src/gu_utils.h
@@ -9,4 +9,27 @@
 #ifndef _gu_utils_h_
 #define _gu_utils_h_
 
+#include <stdbool.h>
+
+/*
+ * The string conversion functions below are slighly customized
+ * versions of standard libc functions designed to undestand 'on'/'off' and
+ * K/M/G size modifiers and the like.
+ *
+ * They return pointer to the next character after conversion:
+ * - if (ret == str) no conversion was made
+ * - if (ret[0] == '\0') whole string was converted */
+
+extern const char*
+gu_str2ll   (const char* str, long long* ll);
+
+extern const char*
+gu_str2dbl  (const char* str, double*    dbl);
+
+extern const char*
+gu_str2bool (const char* str, bool*      b);
+
+extern const char*
+gu_str2ptr  (const char* str, void**     ptr);
+
 #endif /* _gu_utils_h_ */
