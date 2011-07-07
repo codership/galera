cb39cee0d555a5fabd36789dc4b16fe21bf46b9c
Alexey Yurchenko <alexey.yurchenko@codership.com>
2011-07-07 19:21:25 +0000

    Refs #495 - made string->bool conversion more sophisticated.
    git-svn-id: https://devel.codership.com/svn/galera/branches/0.8@2234 9148e567-b586-4176-b9b8-4e36cb4ec61b

---
+++ galerautils/src/gu_utils++.cpp
@@ -0,0 +1,50 @@
+// Copyright (C) 2009-2011 Codership Oy <info@codership.com>
+
+/**
+ * @file General-purpose functions and templates
+ *
+ * $Id$
+ */
+
+#include "gu_utils.hpp"
+
+#include <algorithm>
+
+namespace gu {
+
+bool
+_to_bool (const std::string& s) throw(NotFound)
+{
+    std::istringstream iss(s);
+    bool               ret;
+
+    if ((iss >> ret).fail())
+    {
+        /* if 1|0 didn't work, try true|false */
+        iss.clear();
+        iss.seekg(0);
+
+        if ((iss >> std::boolalpha >> ret).fail())
+        {
+            /* try on/off and yes/no */
+            std::string tmp(s);
+
+            gu::trim(tmp);
+
+            if (tmp.length() >=2 && tmp.length() <= 3)
+            {
+                std::transform (tmp.begin(), tmp.end(), tmp.begin(),
+                                (int(*)(int)) std::tolower);
+
+                if (tmp == "yes" || tmp == "on") return true;
+                if (tmp == "off" || tmp == "no") return false;
+            }
+
+            throw NotFound();
+        }
+    }
+
+    return ret;
+}
+
+} // namespace gu
