2b2b7b61f8a126c874908923b3423bf365441b27
Alexey Yurchenko <alexey.yurchenko@codership.com>
2011-08-15 07:33:07 +0000

    Refs #524 - wrote a proper unit test for gcs_state_msg unit and fixed a bunch of bugs as a result.
    git-svn-id: https://devel.codership.com/svn/galera/branches/0.8@2308 9148e567-b586-4176-b9b8-4e36cb4ec61b

--- gcs/src/unit_tests/gcs_state_msg_test.c
+++ gcs/src/unit_tests/gcs_state_msg_test.c
@@ -87,29 +87,339 @@ START_TEST (gcs_state_msg_test_basic)
 }
 END_TEST
 
-START_TEST (gcs_state_msg_test_quorum)
+START_TEST (gcs_state_msg_test_quorum_inherit)
 {
-    gcs_state_msg_t* st1, *st2, *st3;
-    gu_uuid_t        g1, g2, g3;
+    gcs_state_msg_t* st[3] = { NULL, };
 
-    gu_uuid_generate (&g1, NULL, 0);
-    gu_uuid_generate (&g2, NULL, 0);
-    gu_uuid_generate (&g3, NULL, 0);
+    gu_uuid_t state_uuid;
+    gu_uuid_t group1_uuid, group2_uuid;
+    gu_uuid_t prim1_uuid, prim2_uuid;
 
-    st1 = st2 = st3 = NULL;
+    gu_uuid_generate (&state_uuid,  NULL, 0);
+    gu_uuid_generate (&group1_uuid, NULL, 0);
+    gu_uuid_generate (&group2_uuid, NULL, 0);
+    gu_uuid_generate (&prim1_uuid,  NULL, 0);
+    gu_uuid_generate (&prim2_uuid,  NULL, 0);
+
+    gcs_seqno_t prim1_seqno = 123;
+    gcs_seqno_t prim2_seqno = 834;
+
+    gcs_seqno_t act1_seqno = 345;
+    gcs_seqno_t act2_seqno = 239472508908LL;
+
+    gcs_state_quorum_t quorum;
 
     mark_point();
+
+    /* First just nodes from different groups and configurations, none JOINED */
+    st[0] = gcs_state_msg_create (&state_uuid, &group2_uuid, &prim2_uuid,
+                                  5, prim2_seqno - 1, act2_seqno - 1,
+                                  GCS_NODE_STATE_PRIM, GCS_NODE_STATE_PRIM,
+                                  "node0", "",
+                                  0, 1, 1, 0);
+    fail_if(NULL == st[0]);
+
+    st[1] = gcs_state_msg_create (&state_uuid, &group1_uuid, &prim1_uuid,
+                                  3, prim1_seqno, act1_seqno,
+                                  GCS_NODE_STATE_PRIM, GCS_NODE_STATE_PRIM,
+                                  "node1", "",
+                                  0, 1, 0, 0);
+    fail_if(NULL == st[1]);
+
+    st[2] = gcs_state_msg_create (&state_uuid, &group2_uuid, &prim2_uuid,
+                                  5, prim2_seqno, act2_seqno,
+                                  GCS_NODE_STATE_PRIM, GCS_NODE_STATE_PRIM,
+                                  "node2", "",
+                                  0, 1, 1, 1);
+    fail_if(NULL == st[2]);
+
+    gu_info ("                  Inherited 1");
+    int ret = gcs_state_msg_get_quorum ((const gcs_state_msg_t**)st,
+                                        sizeof(st)/sizeof(gcs_state_msg_t*),
+                                        &quorum);
+    fail_if (0 != ret);
+    fail_if (1 != quorum.version);
+    fail_if (false != quorum.primary);
+    fail_if (0 != gu_uuid_compare(&quorum.group_uuid, &GU_UUID_NIL));
+    fail_if (GCS_SEQNO_ILL != quorum.act_id);
+    fail_if (GCS_SEQNO_ILL != quorum.conf_id);
+    fail_if (-1 != quorum.gcs_proto_ver);
+    fail_if (-1 != quorum.repl_proto_ver);
+    fail_if (-1 != quorum.appl_proto_ver);
+
+    /* now make node1 inherit PC */
+    gcs_state_msg_destroy (st[1]);
+    st[1] = gcs_state_msg_create (&state_uuid, &group1_uuid, &prim1_uuid,
+                                  3, prim1_seqno, act1_seqno,
+                                  GCS_NODE_STATE_JOINED, GCS_NODE_STATE_DONOR,
+                                  "node1", "",
+                                  0, 1, 0, 0);
+    fail_if(NULL == st[1]);
+
+    gu_info ("                  Inherited 2");
+    ret = gcs_state_msg_get_quorum ((const gcs_state_msg_t**)st,
+                                    sizeof(st)/sizeof(gcs_state_msg_t*),
+                                    &quorum);
+    fail_if (0 != ret);
+    fail_if (1 != quorum.version);
+    fail_if (true != quorum.primary);
+    fail_if (0 != gu_uuid_compare(&quorum.group_uuid, &group1_uuid));
+    fail_if (act1_seqno  != quorum.act_id);
+    fail_if (prim1_seqno != quorum.conf_id);
+    fail_if (0 != quorum.gcs_proto_ver);
+    fail_if (1 != quorum.repl_proto_ver);
+    fail_if (0 != quorum.appl_proto_ver);
+
+    /* now make node0 inherit PC (should yield conflicting uuids) */
+    gcs_state_msg_destroy (st[0]);
+    st[0] = gcs_state_msg_create (&state_uuid, &group2_uuid, &prim2_uuid,
+                                  5, prim2_seqno - 1, act2_seqno - 1,
+                                  GCS_NODE_STATE_SYNCED, GCS_NODE_STATE_SYNCED,
+                                  "node0", "",
+                                  0, 1, 1, 0);
+    fail_if(NULL == st[0]);
+
+    gu_info ("                  Inherited 3");
+    ret = gcs_state_msg_get_quorum ((const gcs_state_msg_t**)st,
+                                    sizeof(st)/sizeof(gcs_state_msg_t*),
+                                    &quorum);
+    fail_if (0 != ret);
+    fail_if (1 != quorum.version);
+    fail_if (false != quorum.primary);
+    fail_if (0 != gu_uuid_compare(&quorum.group_uuid, &GU_UUID_NIL));
+    fail_if (GCS_SEQNO_ILL != quorum.act_id);
+    fail_if (GCS_SEQNO_ILL != quorum.conf_id);
+    fail_if (-1 != quorum.gcs_proto_ver);
+    fail_if (-1 != quorum.repl_proto_ver);
+    fail_if (-1 != quorum.appl_proto_ver);
+
+    /* now make node1 non-joined again: group2 should win */
+    gcs_state_msg_destroy (st[1]);
+    st[1] = gcs_state_msg_create (&state_uuid, &group1_uuid, &prim1_uuid,
+                                  3, prim1_seqno, act1_seqno,
+                                  GCS_NODE_STATE_JOINED, GCS_NODE_STATE_PRIM,
+                                  "node1", "",
+                                  0, 1, 0, 0);
+    fail_if(NULL == st[1]);
+
+    gu_info ("                  Inherited 4");
+    ret = gcs_state_msg_get_quorum ((const gcs_state_msg_t**)st,
+                                    sizeof(st)/sizeof(gcs_state_msg_t*),
+                                    &quorum);
+    fail_if (0 != ret);
+    fail_if (1 != quorum.version);
+    fail_if (true != quorum.primary);
+    fail_if (0 != gu_uuid_compare(&quorum.group_uuid, &group2_uuid));
+    fail_if (act2_seqno - 1 != quorum.act_id);
+    fail_if (prim2_seqno - 1 != quorum.conf_id);
+    fail_if (0 != quorum.gcs_proto_ver);
+    fail_if (1 != quorum.repl_proto_ver);
+    fail_if (0 != quorum.appl_proto_ver);
+
+    /* now make node2 joined: it should become a representative */
+    gcs_state_msg_destroy (st[2]);
+    st[2] = gcs_state_msg_create (&state_uuid, &group2_uuid, &prim2_uuid,
+                                  5, prim2_seqno, act2_seqno,
+                                  GCS_NODE_STATE_SYNCED, GCS_NODE_STATE_SYNCED,
+                                  "node2", "",
+                                  0, 1, 1, 0);
+    fail_if(NULL == st[2]);
+
+    gu_info ("                  Inherited 5");
+    ret = gcs_state_msg_get_quorum ((const gcs_state_msg_t**)st,
+                                    sizeof(st)/sizeof(gcs_state_msg_t*),
+                                    &quorum);
+    fail_if (0 != ret);
+    fail_if (1 != quorum.version);
+    fail_if (true != quorum.primary);
+    fail_if (0 != gu_uuid_compare(&quorum.group_uuid, &group2_uuid));
+    fail_if (act2_seqno != quorum.act_id);
+    fail_if (prim2_seqno != quorum.conf_id);
+    fail_if (0 != quorum.gcs_proto_ver);
+    fail_if (1 != quorum.repl_proto_ver);
+    fail_if (0 != quorum.appl_proto_ver);
+
+    gcs_state_msg_destroy (st[0]);
+    gcs_state_msg_destroy (st[1]);
+    gcs_state_msg_destroy (st[2]);
+}
+END_TEST
+
+START_TEST (gcs_state_msg_test_quorum_remerge)
+{
+    gcs_state_msg_t* st[3] = { NULL, };
+
+    gu_uuid_t state_uuid;
+    gu_uuid_t group1_uuid, group2_uuid;
+    gu_uuid_t prim0_uuid, prim1_uuid, prim2_uuid;
+
+    gu_uuid_generate (&state_uuid,  NULL, 0);
+    gu_uuid_generate (&group1_uuid, NULL, 0);
+    gu_uuid_generate (&group2_uuid, NULL, 0);
+    gu_uuid_generate (&prim0_uuid,  NULL, 0);
+    gu_uuid_generate (&prim1_uuid,  NULL, 0);
+    gu_uuid_generate (&prim2_uuid,  NULL, 0);
+
+    gcs_seqno_t prim1_seqno = 123;
+    gcs_seqno_t prim2_seqno = 834;
+
+    gcs_seqno_t act1_seqno = 345;
+    gcs_seqno_t act2_seqno = 239472508908LL;
+
+    gcs_state_quorum_t quorum;
+
+    mark_point();
+
+    /* First just nodes from different groups and configurations, none JOINED */
+    st[0] = gcs_state_msg_create (&state_uuid, &group2_uuid, &prim0_uuid,
+                                  5, prim2_seqno - 1, act2_seqno - 1,
+                                  GCS_NODE_STATE_JOINER,GCS_NODE_STATE_NON_PRIM,
+                                  "node0", "",
+                                  0, 1, 1, 0);
+    fail_if(NULL == st[0]);
+
+    st[1] = gcs_state_msg_create (&state_uuid, &group1_uuid, &prim1_uuid,
+                                  3, prim1_seqno, act1_seqno,
+                                  GCS_NODE_STATE_JOINER,GCS_NODE_STATE_NON_PRIM,
+                                  "node1", "",
+                                  0, 1, 0, 0);
+    fail_if(NULL == st[1]);
+
+    st[2] = gcs_state_msg_create (&state_uuid, &group2_uuid, &prim2_uuid,
+                                  5, prim2_seqno, act2_seqno,
+                                  GCS_NODE_STATE_JOINER,GCS_NODE_STATE_NON_PRIM,
+                                  "node2", "",
+                                  0, 1, 1, 1);
+    fail_if(NULL == st[2]);
+
+    gu_info ("                  Remerged 1");
+    int ret = gcs_state_msg_get_quorum ((const gcs_state_msg_t**)st,
+                                        sizeof(st)/sizeof(gcs_state_msg_t*),
+                                        &quorum);
+    fail_if (0 != ret);
+    fail_if (1 != quorum.version);
+    fail_if (false != quorum.primary);
+    fail_if (0 != gu_uuid_compare(&quorum.group_uuid, &GU_UUID_NIL));
+    fail_if (GCS_SEQNO_ILL != quorum.act_id);
+    fail_if (GCS_SEQNO_ILL != quorum.conf_id);
+    fail_if (-1 != quorum.gcs_proto_ver);
+    fail_if (-1 != quorum.repl_proto_ver);
+    fail_if (-1 != quorum.appl_proto_ver);
+
+    /* Now make node0 to be joined at least once */
+    gcs_state_msg_destroy (st[0]);
+    st[0] = gcs_state_msg_create (&state_uuid, &group2_uuid, &prim0_uuid,
+                                  5, prim2_seqno - 1, act2_seqno - 1,
+                                  GCS_NODE_STATE_DONOR, GCS_NODE_STATE_NON_PRIM,
+                                  "node0", "",
+                                  0, 1, 1, 0);
+    fail_if(NULL == st[0]);
+
+    gu_info ("                  Remerged 2");
+    ret = gcs_state_msg_get_quorum ((const gcs_state_msg_t**)st,
+                                    sizeof(st)/sizeof(gcs_state_msg_t*),
+                                    &quorum);
+    fail_if (0 != ret);
+    fail_if (1 != quorum.version);
+    fail_if (true != quorum.primary);
+    fail_if (0 != gu_uuid_compare(&quorum.group_uuid, &group2_uuid));
+    fail_if (act2_seqno - 1 != quorum.act_id);
+    fail_if (prim2_seqno - 1 != quorum.conf_id);
+    fail_if (0 != quorum.gcs_proto_ver);
+    fail_if (1 != quorum.repl_proto_ver);
+    fail_if (0 != quorum.appl_proto_ver);
+
+    /* Now make node2 to be joined too */
+    gcs_state_msg_destroy (st[2]);
+    st[2] = gcs_state_msg_create (&state_uuid, &group2_uuid, &prim2_uuid,
+                                  5, prim2_seqno, act2_seqno,
+                                  GCS_NODE_STATE_JOINED,GCS_NODE_STATE_NON_PRIM,
+                                  "node2", "",
+                                  0, 1, 1, 1);
+    fail_if(NULL == st[2]);
+
+    gu_info ("                  Remerged 3");
+    ret = gcs_state_msg_get_quorum ((const gcs_state_msg_t**)st,
+                                    sizeof(st)/sizeof(gcs_state_msg_t*),
+                                    &quorum);
+    fail_if (0 != ret);
+    fail_if (1 != quorum.version);
+    fail_if (true != quorum.primary);
+    fail_if (0 != gu_uuid_compare(&quorum.group_uuid, &group2_uuid));
+    fail_if (act2_seqno != quorum.act_id);
+    fail_if (prim2_seqno != quorum.conf_id);
+    fail_if (0 != quorum.gcs_proto_ver);
+    fail_if (1 != quorum.repl_proto_ver);
+    fail_if (0 != quorum.appl_proto_ver);
+
+    /* now make node1 joined too: conflict */
+    gcs_state_msg_destroy (st[1]);
+    st[1] = gcs_state_msg_create (&state_uuid, &group1_uuid, &prim1_uuid,
+                                  3, prim1_seqno, act1_seqno,
+                                  GCS_NODE_STATE_SYNCED,GCS_NODE_STATE_NON_PRIM,
+                                  "node1", "",
+                                  0, 1, 0, 0);
+    fail_if(NULL == st[1]);
+
+    gu_info ("                  Remerged 4");
+    ret = gcs_state_msg_get_quorum ((const gcs_state_msg_t**)st,
+                                    sizeof(st)/sizeof(gcs_state_msg_t*),
+                                    &quorum);
+    fail_if (0 != ret);
+    fail_if (1 != quorum.version);
+    fail_if (false != quorum.primary);
+    fail_if (0 != gu_uuid_compare(&quorum.group_uuid, &GU_UUID_NIL));
+    fail_if (GCS_SEQNO_ILL != quorum.act_id);
+    fail_if (GCS_SEQNO_ILL != quorum.conf_id);
+    fail_if (-1 != quorum.gcs_proto_ver);
+    fail_if (-1 != quorum.repl_proto_ver);
+    fail_if (-1 != quorum.appl_proto_ver);
+
+    /* now make node1 current joiner: should be ignored */
+    gcs_state_msg_destroy (st[1]);
+    st[1] = gcs_state_msg_create (&state_uuid, &group1_uuid, &prim1_uuid,
+                                  3, prim1_seqno, act1_seqno,
+                                  GCS_NODE_STATE_SYNCED, GCS_NODE_STATE_JOINER,
+                                  "node1", "",
+                                  0, 1, 0, 0);
+    fail_if(NULL == st[1]);
+
+    gu_info ("                  Remerged 5");
+    ret = gcs_state_msg_get_quorum ((const gcs_state_msg_t**)st,
+                                    sizeof(st)/sizeof(gcs_state_msg_t*),
+                                    &quorum);
+    fail_if (0 != ret);
+    fail_if (1 != quorum.version);
+    fail_if (true != quorum.primary);
+    fail_if (0 != gu_uuid_compare(&quorum.group_uuid, &group2_uuid));
+    fail_if (act2_seqno != quorum.act_id);
+    fail_if (prim2_seqno != quorum.conf_id);
+    fail_if (0 != quorum.gcs_proto_ver);
+    fail_if (1 != quorum.repl_proto_ver);
+    fail_if (0 != quorum.appl_proto_ver);
+
+    gcs_state_msg_destroy (st[0]);
+    gcs_state_msg_destroy (st[1]);
+    gcs_state_msg_destroy (st[2]);
 }
 END_TEST
 
 Suite *gcs_state_msg_suite(void)
 {
   Suite *s  = suite_create("GCS state message");
-  TCase *tc = tcase_create("gcs_state_msg");
+  TCase *tc_basic   = tcase_create("gcs_state_msg_basic");
+  TCase *tc_inherit = tcase_create("gcs_state_msg_inherit");
+  TCase *tc_remerge = tcase_create("gcs_state_msg_remerge");
 
-  suite_add_tcase (s, tc);
-  tcase_add_test  (tc, gcs_state_msg_test_basic);
-  tcase_add_test  (tc, gcs_state_msg_test_quorum);
+  suite_add_tcase (s, tc_basic);
+  tcase_add_test  (tc_basic, gcs_state_msg_test_basic);
+
+  suite_add_tcase (s, tc_inherit);
+  tcase_add_test  (tc_inherit, gcs_state_msg_test_quorum_inherit);
+
+  suite_add_tcase (s, tc_remerge);
+  tcase_add_test  (tc_remerge, gcs_state_msg_test_quorum_remerge);
+
   return s;
 }
 
