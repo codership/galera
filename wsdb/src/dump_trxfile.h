7e81b9e1502a6c4b729379b7c32e90ca9258e653
Alexey Yurchenko <alexey.yurchenko@codership.com>
2011-08-31 12:56:13 +0000

    Refs #505 - moved old stuff from /trunk to /pasture
    git-svn-id: https://devel.codership.com/svn/galera/trunk@2334 9148e567-b586-4176-b9b8-4e36cb4ec61b

--- wsdb/src/dump_trxfile.h
+++
@@ -1,8 +0,0 @@
-// Copyright (C) 2007 Codership Oy <info@codership.com>
-
-#ifndef DUMP_TRXFILE_INCLUDED
-#define DUMP_TRX_FILE_INCLUDED
-
-int dump_trx_files(const char *work_dir, const char *base_name);
-
-#endif
