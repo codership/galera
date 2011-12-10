ad40fcc3a2961776170b8d514781ab51107b4cef
Alexey Yurchenko <alexey.yurchenko@codership.com>
2011-12-10 03:03:10 +0000

    Refs #612 - added regression test for lp:900816
    git-svn-id: https://devel.codership.com/svn/galera/branches/1.x@2607 9148e567-b586-4176-b9b8-4e36cb4ec61b

---
+++ tests/regressions/lp900816/run.sh
@@ -0,0 +1,31 @@
+#!/bin/sh -uex
+
+PORT=3305
+HOST=127.0.0.1
+USER=root
+PSWD=rootpass
+DB=test
+TABLE=nopk
+TRIES=1000
+
+MYSQL="mysql -u$USER -p$PSWD -h$HOST -P$PORT $DB"
+
+CREATE="
+DROP TABLE IF EXISTS $DB.$TABLE;
+CREATE TABLE $DB.$TABLE (i INT, j INT);"
+
+INSERT="INSERT INTO $DB.$TABLE VALUES (1, 0),(2,0);"
+
+UPDATE="UPDATE $DB.$TABLE SET j=j+1;"
+
+DELETE="DELETE FROM $DB.$TABLE;"
+
+echo $CREATE | $MYSQL
+
+for i in $(seq 1 $TRIES)
+do
+    echo $INSERT | $MYSQL
+    echo $UPDATE | $MYSQL >> update.log 2>&1 &
+    echo $DELETE | $MYSQL
+done
+
