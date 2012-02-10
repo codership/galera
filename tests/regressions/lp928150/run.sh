92f1b4472d0964760a56458da9d2dcea95af2c31
Teemu Ollakka <teemu.ollakka@codership.com>
2012-02-10 13:47:36 +0000

    refs #624, #536 merged [2717] from 1.x to 2.x
    git-svn-id: https://devel.codership.com/svn/galera/branches/2.x@2718 9148e567-b586-4176-b9b8-4e36cb4ec61b

---
+++ tests/regressions/lp928150/run.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#
+# lp:928150
+# https://bugs.launchpad.net/codership-mysql/+bug/928150
+#
+# TEST SETUP
+#
+# This test starts the cluster and runs 100% autocommit load.
+#
+# PARAMETERS
+#
+# Duration of test run
+DURATION=${DURATION:-"3600"}
+#
+#
+
+
+declare -r DIST_BASE=$(cd $(dirname $0)/../..; pwd -P)
+TEST_BASE=${TEST_BASE:-"$DIST_BASE"}
+
+. $TEST_BASE/conf/main.conf
+declare -r SCRIPTS="$DIST_BASE/scripts"
+. $SCRIPTS/jobs.sh
+. $SCRIPTS/action.sh
+. $SCRIPTS/kill.sh
+. $SCRIPTS/misc.sh
+
+echo "##################################################################"
+echo "##             regression test for lp:928150"
+echo "##################################################################"
+
+echo "restarting cluster to clean state"
+restart
+
+
+echo "starting load for $DURATION" seconds
+SQLGEN=${SQLGEN:-"$DIST_BASE/bin/sqlgen"}
+
+$SQLGEN --user $DBMS_TEST_USER --pswd $DBMS_TEST_PSWD --host $DBMS_HOST \
+    --port $DBMS_PORT --users $DBMS_CLIENTS --duration $DURATION \
+    --stat-interval 30 --sess-min 999999 --sess-max 999999 \
+    --rollbacks 0.1 --ac-frac 100
+
+echo "checking consistency"
+check
+
+echo "stopping cluster"
+stop
