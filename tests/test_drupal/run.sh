10bf54b978c259b6789e7be7cd239c9b7ecc707d
Teemu Ollakka <teemu.ollakka@codership.com>
2012-03-26 10:34:32 +0000

    refs #652 - added test script to run drupal install and test suite automatically
    git-svn-id: https://devel.codership.com/svn/galera/branches/2.x@2756 9148e567-b586-4176-b9b8-4e36cb4ec61b

---
+++ tests/test_drupal/run.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+
+#
+# DESCRIPTION:
+#
+# This scripts installs drupal site and runs drupal test suite against
+# installed site. Drupal package will be fetched automatically if it is
+# not found from working directory.
+#
+# SETUP:
+#
+# Simplest way to get test running is to create symbolic link
+# /var/www/drupal_test which will point to ./workdir under this directory.
+#
+# PARAMETERS:
+#
+# CONCURRENCY    - number of test cases run concurrently (default 1)
+# WORKDIR        - directory where drupal site is extracted and installed,
+#                  should reside under www root so that site can be accessed
+#                  via web server (default ./workdir)
+# DRUPAL_VERSION - drupal version to be tested
+# URL            - url pointing to installed site, should be specified only
+#                  if default does not work
+# TESTS          - drupal test suites to be run or --all to run all tests
+#
+# NOTES:
+# * Many tests will fail. However, if site is installed through browser
+#   instead of drush, these tests will pass. Exact reason for this is
+#   still unknown.
+#
+# WARNINGS:
+# * Drops database drupal before site is installed
+#
+
+
+set -e
+
+BASE_DIR=$(cd $(dirname $0); pwd -P)
+
+# Parameters
+CONCURRENCY=${CONCURRENCY:-"1"}
+WORKDIR=${WORKDIR:-"$BASE_DIR/workdir"}
+DRUPAL_VERSION=${DRUPAL_VERSION:-"7.12"}
+URL=${URL:-"http://localhost/drupal_test/$DRUPAL_DIR"}
+TESTS=${TESTS:-"--all"}
+
+# Drupal pkg/dirname
+DRUPAL_PKG="drupal-${DRUPAL_VERSION}.tar.gz"
+DRUPAL_DIR="$(basename $DRUPAL_PKG .tar.gz)"
+
+
+PHP=$(which php)
+DRUSH=$(which drush)
+
+if ! test -x $DRUSH
+then
+    echo "Program 'drush' is required to run this test"
+    exit 1
+fi
+
+if ! test -x $PHP
+then
+    echo "PHP is required to run this test"
+    exit 1
+fi
+
+# Helpers to manage the cluster
+declare -r DIST_BASE=$(cd $(dirname $0)/..; pwd -P)
+TEST_BASE=${TEST_BASE:-"$DIST_BASE"}
+
+. $TEST_BASE/conf/main.conf
+declare -r SCRIPTS="$DIST_BASE/scripts"
+
+# MySQL management
+MYSQL="mysql -u${DBMS_ROOT_USER} -p${DBMS_ROOT_PSWD}
+             -h${NODE_INCOMING_HOST[0]} -P${NODE_INCOMING_PORT[0]}"
+
+
+# Create working directory if it does not exist yet
+if ! test -d $WORKDIR
+then
+    mkdir -p $WORKDIR
+fi
+
+cd $WORKDIR
+
+# Download and extract drupal package
+if ! test -f $DRUPAL_PKG
+then
+    echo "-- Downloading drupal package"
+    wget http://ftp.drupal.org/files/projects/${DRUPAL_PKG}
+fi
+
+echo "-- Extracting drupal package"
+tar zxf $DRUPAL_PKG
+cd $DRUPAL_DIR
+
+# Relax permissions for installer
+chmod -R a+rw sites/default
+cp sites/default/default.settings.php sites/default/settings.php
+chmod a+rw sites/default/settings.php
+
+# Restart cluster
+echo "-- Restarting cluster"
+$SCRIPTS/command.sh restart
+
+# Drop drupal database, set globals
+$MYSQL -e "DROP DATABASE IF EXISTS drupal;
+           SET GLOBAL auto_increment_increment=1;
+           SET GLOBAL wsrep_drupal_282555_workaround=0;"
+
+# Install site
+echo "-- Installing drupal"
+$DRUSH si \
+    --db-url="mysql://${DBMS_TEST_USER}:${DBMS_TEST_PSWD}@${NODE_INCOMING_HOST[0]}:${NODE_INCOMING_PORT[0]}/drupal" \
+    --account-name=drupal --account-pass=drupass --clean-url=0 -y
+# Relax permissions so that dir can be cleaned up or overwritten
+# automatically.
+chmod -R a+rw .
+
+# Enable modules
+MODULES=$(ls -1 modules | grep -v README.txt)
+$DRUSH en $MODULES -y
+
+# Run tests
+cd scripts
+echo "-- Running tests $TESTS"
+$PHP run-tests.sh --concurrency $CONCURRENCY --php $PHP --url $URL $TESTS
