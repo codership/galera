7cc42c602d6512b206a1bf70abfc735e22fee506
Alexey Yurchenko <alexey.yurchenko@codership.com>
2012-05-29 14:29:35 +0000

    Refs #690 - added gu_mmh3.h and gu_mmh3.c units
    git-svn-id: https://devel.codership.com/svn/galera/branches/2.x@2791 9148e567-b586-4176-b9b8-4e36cb4ec61b

---
+++ galerautils/src/gu_mmh3.c
@@ -0,0 +1,9 @@
+// Copyright (C) 2012 Codership Oy <info@codership.com>
+
+/**
+ * @file MurMurHash3 implementation
+ *
+ * $Id$
+ */
+
+#include "gu_mmh3.h"
---
+++ galerautils/src/gu_mmh3.h
@@ -0,0 +1,12 @@
+// Copyright (C) 2012 Codership Oy <info@codership.com>
+
+/**
+ * @file MurMurHash3 header
+ *
+ * $Id$
+ */
+
+#ifndef _gu_mmh3_h_
+#define _gu_mmh3_h_
+
+#endif /* _gu_mmh3_h_ */
