64375abfba087f9608d5fc63346755075b15160b
Vladislav Klyachin <vladislav.klyachin@codership.com>
2013-08-28 06:32:31 +0000

    Refs #775: LD_PRELOAD replacement in tests for MacOSX
    git-svn-id: https://devel.codership.com/svn/galera/branches/23.2.x@3236 9148e567-b586-4176-b9b8-4e36cb4ec61b

--- tests/test_stopcont/run.sh
+++ tests/test_stopcont/run.sh
@@ -20,6 +20,8 @@ PAUSE_RND=${3:-"20"}
 # Start load
 SQLGEN=${SQLGEN:-"$DIST_BASE/bin/sqlgen"}
 LD_PRELOAD=$GLB_PRELOAD \
+DYLD_INSERT_LIBRARIES=$GLB_PRELOAD \
+DYLD_FORCE_FLAT_NAMESPACE=1 \
 $SQLGEN --user $DBMS_TEST_USER --pswd $DBMS_TEST_PSWD --host $DBMS_HOST \
         --port $DBMS_PORT --users $DBMS_CLIENTS --duration 999999999 \
         --stat-interval 99999999 >/dev/null 2>$BASE_RUN/seesaw.err &
