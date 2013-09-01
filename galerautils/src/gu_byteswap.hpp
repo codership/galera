f759ffe6114d1bdc4231d24bd253ba5103b9ecd6
Vladislav Klyachin <vladislav.klyachin@codership.com>
2013-09-01 14:59:03 +0000

    Refs #775: one more compiler compatibility fix for r3256
    git-svn-id: https://devel.codership.com/svn/galera/branches/3.x@3258 9148e567-b586-4176-b9b8-4e36cb4ec61b

--- galerautils/src/gu_byteswap.hpp
+++ galerautils/src/gu_byteswap.hpp
@@ -12,7 +12,6 @@
 #include "gu_byteswap.h"
 
 #include <stdint.h>
-#include <limits.h>
 
 namespace gu
 {
@@ -41,16 +40,18 @@ template <> GU_FORCE_INLINE unsigned int gtoh (const unsigned int& val)
     return  gtoh32(val);
 }
 
-#if ULONG_MAX == UINT32_MAX
+#if __LONG_MAX__ == __INT_MAX__
 template <> GU_FORCE_INLINE unsigned long gtoh (const unsigned long& val)
 {
     return  gtoh32(val);
 }
-#elif ULONG_MAX == UINT64_MAX
+#elif __LONG_MAX__ == __LONG_LONG_MAX__
 template <> GU_FORCE_INLINE unsigned long gtoh (const unsigned long& val)
 {
     return  gtoh64(val);
 }
+#else
+# error can not determine size of long
 #endif
 
 template <> GU_FORCE_INLINE unsigned long long gtoh (const unsigned long long& val)
