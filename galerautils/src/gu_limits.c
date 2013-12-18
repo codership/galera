cdfb7265a73ff5457f1b05af21f6293b37b5fc9c
Alexey Yurchenko <alexey.yurchenko@codership.com>
2013-12-18 04:30:28 +0000

    Refs #827: merged r3428 from 2.x
    git-svn-id: https://devel.codership.com/svn/galera/branches/3.x@3429 9148e567-b586-4176-b9b8-4e36cb4ec61b

--- galerautils/src/gu_limits.c
+++ galerautils/src/gu_limits.c
@@ -9,12 +9,12 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <sys/sysctl.h>
 #include "gu_limits.h"
 #include "gu_log.h"
 
 #if defined(__APPLE__)
 
+#include <sys/sysctl.h> // doesn't seem to be used directly, but jst in case
 #include <mach/mach.h>
 
 long gu_darwin_phys_pages (void)
@@ -22,14 +22,16 @@ long gu_darwin_phys_pages (void)
     /* Note: singleton pattern would be useful here */
     vm_statistics64_data_t vm_stat;
     unsigned int count = HOST_VM_INFO64_COUNT;
-    kern_return_t ret = host_statistics64 (mach_host_self (), HOST_VM_INFO64, (host_info64_t) &vm_stat, &count);
+    kern_return_t ret = host_statistics64 (mach_host_self (), HOST_VM_INFO64,
+                                           (host_info64_t) &vm_stat, &count);
     if (ret != KERN_SUCCESS)
     {
         gu_error ("host_statistics64 failed with code %d", ret);
         return 0;
     }
     /* This gives a value a little less than physical memory of computer */
-    return vm_stat.free_count + vm_stat.active_count + vm_stat.inactive_count + vm_stat.wire_count;
+    return vm_stat.free_count + vm_stat.active_count + vm_stat.inactive_count
+         + vm_stat.wire_count;
     /* Exact value may be obtain via sysctl ({CTL_HW, HW_MEMSIZE}) */
     /* Note: sysctl is 60% slower compared to host_statistics64 */
 }
@@ -38,13 +40,15 @@ long gu_darwin_avphys_pages (void)
 {
     vm_statistics64_data_t vm_stat;
     unsigned int count = HOST_VM_INFO64_COUNT;
-    kern_return_t ret = host_statistics64 (mach_host_self (), HOST_VM_INFO64, (host_info64_t) &vm_stat, &count);
+    kern_return_t ret = host_statistics64 (mach_host_self (), HOST_VM_INFO64,
+                                           (host_info64_t) &vm_stat, &count);
     if (ret != KERN_SUCCESS)
     {
         gu_error ("host_statistics64 failed with code %d", ret);
         return 0;
     }
-    /* Note: vm_stat.free_count == vm_page_free_count + vm_page_speculative_count */
+    /* Note:
+     * vm_stat.free_count == vm_page_free_count + vm_page_speculative_count */
     return vm_stat.free_count - vm_stat.speculative_count;
 }
 
@@ -52,6 +56,7 @@ long gu_darwin_avphys_pages (void)
 
 #include <vm/vm_param.h> // VM_TOTAL
 #include <sys/vmmeter.h> // struct vmtotal
+#include <sys/sysctl.h>
 
 long gu_freebsd_avphys_pages (void)
 {
@@ -59,16 +64,19 @@ long gu_freebsd_avphys_pages (void)
     /*       2) vm.stats.vm.v_cache_count is potentially free memory too */
     int mib_vm_stats_vm_v_free_count[4];
     size_t mib_sz = 4;
-    int rc = sysctlnametomib ("vm.stats.vm.v_free_count", mib_vm_stats_vm_v_free_count, &mib_sz);
+    int rc = sysctlnametomib ("vm.stats.vm.v_free_count",
+                              mib_vm_stats_vm_v_free_count, &mib_sz);
     if (rc != 0)
     {
-        gu_error ("sysctlnametomib(vm.stats.vm.v_free_count) failed with code %d", rc);
+        gu_error ("sysctlnametomib(vm.stats.vm.v_free_count) failed, code %d",
+                  rc);
         return 0;
     }
 
     unsigned int vm_stats_vm_v_free_count;
     size_t sz = sizeof (vm_stats_vm_v_free_count);
-    rc = sysctl (mib_vm_stats_vm_v_free_count, mib_sz, &vm_stats_vm_v_free_count, &sz, NULL, 0);
+    rc = sysctl (mib_vm_stats_vm_v_free_count, mib_sz,
+                 &vm_stats_vm_v_free_count, &sz, NULL, 0);
     if (rc != 0)
     {
         gu_error ("sysctl(vm.stats.vm.v_free_count) failed with code %d", rc);
