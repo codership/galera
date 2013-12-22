9201967fcc4af9b26e224862bc20819e9873d351
Alexey Yurchenko <alexey.yurchenko@codership.com>
2013-12-22 13:00:28 +0000

    Refs #827:  - added CRC32C_NO_HARDWARE macro to exclude hardware accelerated code when needed  - fixed CRC32C build issues on Solaris by disabling HW code when host CPU does not support it
    git-svn-id: https://devel.codership.com/svn/galera/branches/3.x@3430 9148e567-b586-4176-b9b8-4e36cb4ec61b

--- galerautils/src/gu_crc32c.c
+++ galerautils/src/gu_crc32c.c
@@ -16,16 +16,18 @@ gu_crc32c_configure()
 {
     gu_crc32c_func = detectBestCRC32C();
 
+#if !defined(CRC32C_NO_HARDWARE)
     if (gu_crc32c_func == crc32cHardware64 ||
         gu_crc32c_func == crc32cHardware32) {
         gu_info ("CRC-32C: using hardware acceleration.");
     }
-    else if (gu_crc32c_func == crc32cSlicingBy8) {
+    else
+#endif /* !CRC32C_NO_HARDWARE */
+    if (gu_crc32c_func == crc32cSlicingBy8) {
         gu_info ("CRC-32C: using \"slicing-by-8\" algorithm.");
     }
     else {
-        gu_warn ("unexpected CRC-32C implementation.");
+        gu_fatal ("unexpected CRC-32C implementation.");
         abort();
     }
 }
-
