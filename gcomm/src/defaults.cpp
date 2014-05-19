66feaed4be3e5e630b2a7bf0a9330637aca43cb4
dirtysalt <dirtysalt1987@gmail.com>
2014-05-19 22:25:31 +0800

    Refs #10: pc.recovery turned on by default

--- gcomm/src/defaults.cpp
+++ gcomm/src/defaults.cpp
@@ -45,5 +45,5 @@ namespace gcomm
     std::string const Defaults::PcWaitPrim              = "true";
     std::string const Defaults::PcWaitPrimTimeout       = "P30S";
     std::string const Defaults::PcWeight                = "1";
-    std::string const Defaults::PcRecovery              = "0";
+    std::string const Defaults::PcRecovery              = "1";
 }
