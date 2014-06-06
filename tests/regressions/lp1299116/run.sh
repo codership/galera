231e52ee35d4458e5e22a4807a7bb91917750285
Teemu Ollakka <teemu.ollakka@codership.com>
2014-06-06 15:25:01 +0300

    refs #30 - manually patched lp1299116/run to match version in 3.x.

--- tests/regressions/lp1299116/run.sh
+++ tests/regressions/lp1299116/run.sh
@@ -11,15 +11,99 @@ MYSQL="mysql -utest -ptestpass -hgw -Dtest"
 MYSQL0="$MYSQL $NODE0"
 MYSQL1="$MYSQL $NODE1"
 
-$MYSQL0 -e "drop table if exists uniq;"
-$MYSQL0 -e "create table uniq (u varchar(10), unique key unique_key(u));"
+$SCRIPTS/command.sh restart
 
-for i in $(seq 1 10)
-do
-    $MYSQL0 -e "insert into uniq (u) values ('const');" &
-    $MYSQL1 -e "insert into uniq (u) values ('const');" &
+test_1()
+{
+    echo 
+    echo "Test 1 starting..."; 
+    echo
+    $MYSQL0 -e "drop table if exists uniq;"
+    $MYSQL0 -e "create table uniq (u varchar(10), unique key unique_key(u));"
+
+    for i in $(seq 1 10)
+    do
+	$MYSQL0 -e "insert into uniq (u) values ('const');" &
+	$MYSQL1 -e "insert into uniq (u) values ('const');" &
+	wait
+    done
+    echo
+    echo "Test 1 done"
+    echo "****************************************************"
+}
+
+test_2_load()
+{
+    local node="${@:1}"
+
+    echo "load 2 for $node starting..."; 
+
+    for i in {1..10000}; do 
+	$MYSQL $node -e"insert into uniq(u) values('keys'); 
+                          delete from uniq;"; 
+    done
+}
+
+test_2()
+{
+    $MYSQL0 -e "drop table if exists uniq;"
+    $MYSQL0 -e "create table uniq (
+                  u varchar(10),
+                  i int auto_increment,
+                  key(i),
+                  unique key unique_key(u));"
+    test_2_load  "$NODE0" &
+    test_2_load  "$NODE1" &
+
+    echo "test 2 loads started"
+
     wait
-done
+    echo
+    echo "Test 2 done"
+    echo "****************************************************"
+}
+
+test_3_load()
+{
+    echo "test load 3"; 
+    for i in $(seq 1 1000)
+    do
+        #echo "round: $i"
+	$MYSQL0 -e "insert into uniq (u) values ('const');" &
+	$MYSQL1 -e "insert into uniq (u) values ('const');" &
+	wait
+	val0=$($MYSQL0 -N -s  -e "select count(*) from uniq;")
+	val1=$($MYSQL1 -N -s  -e "select count(*) from uniq;")
+
+        #sleep 1
+
+	[ "$val0" != "1" ] && echo "0 $val0 $val1" && exit
+	[ "$val1" != "1" ] && echo "1 $val0 $val1" && exit
+
+	echo "truncing"
+	$MYSQL0 -e "truncate uniq;"
+    done
+}
+
+test_3()
+{
+    echo 
+    echo "Test 3 starting..."; 
+    echo
+    $MYSQL0 -e "drop table if exists uniq;"
+    $MYSQL0 -e "create table uniq (u varchar(10), unique key unique_key(u));"
+
+    test_3_load
+    echo
+    echo "Test 3 done"
+    echo "****************************************************"
+}
+
+test_1
+test_2
+test_3
+
+$SCRIPTS/command.sh wait_sync 0 1
 
 test $($MYSQL0 -ss -e "select count(*) from uniq") == 1 || \
     (echo "duplicate uniq key" && exit 1)
@@ -28,3 +112,4 @@ test $($MYSQL1 -ss -e "select count(*) from uniq") == 1 || \
 
 $SCRIPTS/command.sh check | wc -l
 
+$SCRIPTS/command.sh stop
