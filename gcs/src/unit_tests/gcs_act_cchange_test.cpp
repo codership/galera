abb34c1c1c6c05b81dd29332921d72647a8954fa
Alexey Yurchenko <ayurchen@gmail.com>
2015-03-22 13:51:58 +0200

    Refs codership/galera-features#49 - beefed up unit test for CC event

--- gcs/src/unit_tests/gcs_act_cchange_test.cpp
+++ gcs/src/unit_tests/gcs_act_cchange_test.cpp
@@ -10,6 +10,7 @@
 #include "gcs_act_cchange_test.hpp"
 #include "../gcs.hpp"
 #include "gu_uuid.hpp"
+#include "gu_utils.hpp"
 
 
 #include <cstdlib>
@@ -62,7 +63,18 @@ START_TEST (serialization)
     cc_src.repl_proto_ver = 4;
     cc_src.appl_proto_ver = 5;
 
-    // TODO - add memb array
+    for (int i(0); i < 128; ++i) // make really big cluster ;)
+    {
+        gcs_act_cchange::member m;
+
+        gu_uuid_generate(&m.uuid_, &i, sizeof(i));
+        m.name_     = std::string("node") + gu::to_string(i);
+        m.incoming_ = std::string("192.168.0.") + gu::to_string(i) + ":4567";
+        m.cached_   = i % 7 + 47; // some random number
+        m.state_    = gcs_node_state(i % GCS_NODE_STATE_MAX);
+
+        cc_src.memb.push_back(m);
+    }
 
     size = cc_src.write(&buf);
 
@@ -80,6 +92,20 @@ START_TEST (serialization)
         fail_if(cc_dst.repl_proto_ver != cc_src.repl_proto_ver);
         fail_if(cc_dst.appl_proto_ver != cc_src.appl_proto_ver);
     }
+
+    /* another buffer corruption, exception must be thrown */
+    try
+    {
+        static_cast<char*>(buf)[size/2] += 1;
+
+        gcs_act_cchange const cc_dst(buf, size);
+
+        fail_if(true, "exception must be thrown");
+    }
+    catch (gu::Exception& e)
+    {}
+
+    ::free(buf);
 }
 END_TEST
 
