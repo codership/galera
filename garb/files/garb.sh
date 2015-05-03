b0d26c1ea629df196cfbdcbcd2f2085c9935bfae
Alexey Yurchenko <ayurchen@gmail.com>
2015-05-04 01:33:06 +0300

    Fixes codership/galera#186 - all sorts of problems with garbd system scripts   - removes pre-3.x nc/nmap probing for live nodes in garbd system scripts   - encloses garbd parameters in single quotes and makes sure they are passed properly to the daemon start function   - removes final exit 0 in garb.sh that masked real error code   - makes sure to return error if there is no process or PID file can't be created   - removes stale PID file if found on startup instead of freaking out and forcing the user to do it manually   - removes dependency on nc/nmap from RPM spec
    Note that service garb start will return 0 before garbd successfuly joins the
    cluster. This is due to both potentially long wait times and forking into
    daemon before the actual library initialization due to its multithreaded
    character. However cluster connection errors will be logged to syslog/logfile.
    
    Any firther improvements in error code reporting will require garbd code
    refactoring.

--- garb/files/garb.sh
+++ garb/files/garb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (C) 2012-2013 Codership Oy <info@codership.com>
+# Copyright (C) 2012-2015 Codership Oy <info@codership.com>
 #
 # init.d script for garbd
 #
@@ -49,19 +49,23 @@ program_start() {
 	local rcode
 	if [ -f /etc/redhat-release ]; then
 		echo -n $"Starting $prog: "
-                daemon --user nobody $prog $* >/dev/null
+		daemon --user nobody $prog "$@" >/dev/null
 		rcode=$?
-		[ $rcode -eq 0 ] && pidof $prog > $PIDFILE \
-		&& echo_success || echo_failure
+		if [ $rcode -eq 0 ]; then
+			pidof $prog > $PIDFILE || rcode=$?
+		fi
+		[ $rcode -eq 0 ] && echo_success || echo_failure
 		echo
 	else
 		log_daemon_msg "Starting $prog: "
 		start-stop-daemon --start --quiet -c nobody --background \
-		                  --exec $prog -- $*
+		                  --exec $prog -- "$@"
 		rcode=$?
 		# Hack: sleep a bit to give garbd some time to fork
 		sleep 1
-		[ $rcode -eq 0 ] && pidof $prog > $PIDFILE
+		if [ $rcode -eq 0 ]; then
+			pidof $prog > $PIDFILE || rcode=$?
+		fi
 		log_end_msg $rcode
 	fi
 	return $rcode
@@ -74,7 +78,6 @@ program_stop() {
 		killproc -p $PIDFILE
 		rcode=$?
 		[ $rcode -eq 0 ] && echo_success || echo_failure
-#		echo
 	else
 		start-stop-daemon --stop --quiet --oknodo --retry TERM/30/KILL/5 \
 		                  --pidfile $PIDFILE
@@ -103,8 +106,13 @@ start() {
 	fi
 
 	if [ -r $PIDFILE ]; then
-		log_failure "$prog is already running with PID $(cat ${PIDFILE})"
-		return 3 # ESRCH
+		local PID=$(cat ${PIDFILE})
+		if ps -p $PID >/dev/null 2>&1; then
+			log_failure "$prog is already running with PID $PID"
+			return 3 # ESRCH
+		else
+			rm -f $PIDFILE
+		fi
 	fi
 
 	[ -x $prog ] || return 5
@@ -121,31 +129,14 @@ start() {
 
 	GALERA_PORT=${GALERA_PORT:-4567}
 
-	# Find a working node
-	for ADDRESS in ${GALERA_NODES} 0; do
-		HOST=$(echo $ADDRESS | cut  -d \: -f 1 )
-		PORT=$(echo $ADDRESS | cut -sd \: -f 2 )
-		PORT=${PORT:-$GALERA_PORT}
-		if [[ -x `which nc` ]] && nc -h 2>&1 | grep -q  -- '-z';then
-                    nc -z $HOST $PORT >/dev/null && break
-                elif [[ -x `which nmap` ]];then
-                    nmap -Pn -p$PORT $HOST | awk "\$1 ~ /$PORT/ {print \$2}" | grep -q open && break
-                else
-                    log_failure "Neither netcat nor nmap are present for zero I/O scanning"
-                    return 1
-                fi
-	done
-	if [ ${ADDRESS} == "0" ]; then
-		log_failure "None of the nodes in $GALERA_NODES is accessible"
-		return 1
-	fi
+	OPTIONS="-d -a gcomm://${GALERA_NODES// /,}"
+	# substitute space with comma for backward compatibility
 
-	OPTIONS="-d -a gcomm://$ADDRESS"
-	[ -n "$GALERA_GROUP" ]   && OPTIONS="$OPTIONS -g $GALERA_GROUP"
-	[ -n "$GALERA_OPTIONS" ] && OPTIONS="$OPTIONS -o $GALERA_OPTIONS"
-	[ -n "$LOG_FILE" ]       && OPTIONS="$OPTIONS -l $LOG_FILE"
+	[ -n "$GALERA_GROUP" ]   && OPTIONS="$OPTIONS -g '$GALERA_GROUP'"
+	[ -n "$GALERA_OPTIONS" ] && OPTIONS="$OPTIONS -o '$GALERA_OPTIONS'"
+	[ -n "$LOG_FILE" ]       && OPTIONS="$OPTIONS -l '$LOG_FILE'"
 
-	program_start $OPTIONS
+	eval program_start $OPTIONS
 }
 
 stop() {
@@ -184,4 +175,3 @@ case "$1" in
 	exit 2
 esac
 
-exit 0
