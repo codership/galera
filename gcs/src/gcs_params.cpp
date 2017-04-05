ab7f8f5cfbd5607dea388da5ee2864072aef375d
Alexey Yurchenko <ayurchen@galeracluster.com>
2017-04-05 12:54:51 +0100

    GAL-505 use gcs_fc_hard_limit_fix consistently instead of a literal

--- gcs/src/gcs_params.cpp
+++ gcs/src/gcs_params.cpp
@@ -1,10 +1,11 @@
 /*
- * Copyright (C) 2010-2014 Codership Oy <info@codership.com>
+ * Copyright (C) 2010-2017 Codership Oy <info@codership.com>
  *
  * $Id$
  */
 
 #include "gcs_params.hpp"
+#include "gcs_fc.hpp" // gcs_fc_hard_limit_fix
 
 #define __STDC_FORMAT_MACROS
 #include <inttypes.h>
@@ -205,7 +206,8 @@ gcs_params_init (struct gcs_params* params, gu_config_t* config)
     int64_t tmp;
     if ((ret = params_init_int64 (config, GCS_PARAMS_RECV_Q_HARD_LIMIT, 0, 0,
                                   &tmp))) return ret;
-    params->recv_q_hard_limit = tmp * 0.9; // allow for some meta overhead
+    params->recv_q_hard_limit = tmp * gcs_fc_hard_limit_fix;
+    // allow for some meta overhead
 
     if ((ret = params_init_bool (config, GCS_PARAMS_FC_MASTER_SLAVE,
                                  &params->fc_master_slave))) return ret;
