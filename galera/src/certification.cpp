codership/galera

commit 72127261710b747633bac7bbb10cac8d9730b34f

Refs codership/galera-bugs#181 Improved optimistic_pa support
 - Removed marshalling and checking optimistic_pa value for every key:
   it is enough to set the lower limit on depends_seqno_ at trx level.
 - Fixed updating of 'cert.*' parameters in the parameter list.
 - Fixed parsing of boolen 'yes' value (somehow had no effect)

galera/src/certification.cpp
@@ -549,10 +549,9 @@ static inline bool
 certify_and_depend_v3to4(const galera::KeyEntryNG*   const found,
                          const galera::KeySet::KeyPart&    key,
                          galera::TrxHandle*          const trx,
-                         bool                        const log_conflict,
-                         bool                        const optimistic_pa)
+                         bool                        const log_conflict)
 {
-    wsrep_seqno_t depends_seqno(optimistic_pa ? -1 : trx->last_seen_seqno());
+    wsrep_seqno_t depends_seqno(trx->depends_seqno());
     wsrep_key_type_t const key_type(key.wsrep_type(trx->version()));
 
     /*
@@ -583,7 +582,8 @@ certify_and_depend_v3to4(const galera::KeyEntryNG*   const found,
     }
     else
     {
-        trx->set_depends_seqno(std::max(trx->depends_seqno(), depends_seqno));
+        if (depends_seqno > trx->depends_seqno())
+            trx->set_depends_seqno(depends_seqno);
         return false;
     }
 }
@@ -594,8 +594,7 @@ certify_v3to4(galera::Certification::CertIndexNG& cert_index_ng,
               const galera::KeySet::KeyPart&      key,
               galera::TrxHandle*                  trx,
               bool const                          store_keys,
-              bool const                          log_conflicts,
-              bool const                          optimistic_pa)
+              bool const                          log_conflicts)
 {
     galera::KeyEntryNG ke(key);
     galera::Certification::CertIndexNG::iterator ci(cert_index_ng.find(&ke));
@@ -619,8 +618,7 @@ certify_v3to4(galera::Certification::CertIndexNG& cert_index_ng,
         // Note: For we skip certification for isolated trxs, only
         // cert index and key_list is populated.
         return (!trx->is_toi() &&
-                certify_and_depend_v3to4(kep, key, trx, log_conflicts,
-                                         optimistic_pa));
+                certify_and_depend_v3to4(kep, key, trx, log_conflicts));
     }
 }
 
@@ -646,8 +644,7 @@ galera::Certification::do_test_v3to4(TrxHandle* trx, bool store_keys)
     {
         const KeySet::KeyPart& key(key_set.next());
 
-        if (certify_v3to4(cert_index_ng_, key, trx, store_keys, log_conflicts_,
-                          optimistic_pa_))
+        if (certify_v3to4(cert_index_ng_, key, trx, store_keys, log_conflicts_))
         {
             goto cert_fail;
         }
@@ -807,6 +804,10 @@ galera::Certification::do_test(TrxHandle* trx, bool store_keys)
     {
         trx->set_depends_seqno(
             trx_map_.begin()->second->global_seqno() - 1);
+
+        if (optimistic_pa_ == false &&
+            trx->last_seen_seqno() > trx->depends_seqno())
+            trx->set_depends_seqno(trx->last_seen_seqno());
     }
 
     switch (version_)

galera/src/replicator_smm_params.cpp
@@ -210,18 +210,17 @@ galera::ReplicatorSMM::param_set (const std::string& key,
     {
         found = true;
         set_param (key, value);
-        config_.set (key, value);
     }
 
     if (key == Certification::PARAM_LOG_CONFLICTS)
     {
         cert_.set_log_conflicts(value);
-        return;
+        found = true;
     }
     else if (key == Certification::PARAM_OPTIMISTIC_PA)
     {
         cert_.set_optimistic_pa(value);
-        return;
+        found = true;
     }
     // this key might be for another module
     else if (0 != key.find(common_prefix))
@@ -242,6 +241,8 @@ galera::ReplicatorSMM::param_set (const std::string& key,
     }
 
     if (!found) throw gu::NotFound();
+
+    config_.set(key, value);
 }
 
 std::string

galerautils/src/gu_utils.c
@@ -69,8 +69,8 @@ gu_str2dbl (const char* str, double* dbl)
 const char*
 gu_str2bool (const char* str, bool* b)
 {
-    size_t len = strlen(str);
-    int    res = -1; /* no conversion */
+    size_t const len = strlen(str);
+    int res = -1; /* no conversion */
 
     switch (len)
     {
@@ -82,17 +82,19 @@ gu_str2bool (const char* str, bool* b)
         }
         break;
     case 2:
-        if (!strcasecmp(str, "on")) res = 1;
-        if (!strcasecmp(str, "no")) res = 0;
+        if      (!strcasecmp(str, "on")) res = 1;
+        else if (!strcasecmp(str, "no")) res = 0;
         break;
     case 3:
-        if (!strcasecmp(str, "off")) res = 0;
-        if (!strcasecmp(str, "yes")) res = 1;
+        if      (!strcasecmp(str, "off")) res = 0;
+        else if (!strcasecmp(str, "yes")) res = 1;
         break;
     case 4:
-        if (!strcasecmp(str, "true")) res = 1;
-        if (!strcasecmp(str, "sure")) res = 1;
-        if (!strcasecmp(str, "nope")) res = 0;
+        if      (!strcasecmp(str, "true")) res = 1;
+        else if (!strcasecmp(str, "sure")) res = 1;
+        else if (!strcasecmp(str, "none")) res = 0;
+        else if (!strcasecmp(str, "nope")) res = 0;
+        else if (!strcasecmp(str, "yeah")) res = 1;
         break;
     case 5:
         if (!strcasecmp(str, "false")) res = 0;

commit 6917229ce9b440dcfdaebb39964f6e3c8741cdb0

Refs codership/galera-bugs#181 Fixup to regressions in previous commit
Commit 72127261710b747633bac7bbb10cac8d9730b34f introduced a regression
where it unconditionally saves any changed parameter, including those
intended to be used as control sequences and not intended to be saved.
Reversed that leaving each module to deal with its own parameters as
they see fit.

galera/src/certification.cpp
@@ -884,6 +884,7 @@ galera::Certification::do_test_preordered(TrxHandle* trx)
 galera::Certification::Certification(gu::Config& conf, ServiceThd& thd)
     :
     version_               (-1),
+    conf_                  (conf),
     trx_map_               (),
     cert_index_            (),
     cert_index_ng_         (),
@@ -1177,7 +1178,7 @@ set_boolean_parameter(bool& param,
         param = gu::Config::from_config<bool>(value);
         if (old != param)
         {
-            log_info << (param ? "Enabled" : "Disabled") << change_msg;
+            log_info << (param ? "Enabled " : "Disabled ") << change_msg;
         }
     }
     catch (gu::NotFound& e)
@@ -1189,16 +1190,23 @@ set_boolean_parameter(bool& param,
 }
 
 void
-galera::Certification::set_log_conflicts(const std::string& str)
+galera::Certification::param_set(const std::string& key,
+                                 const std::string& value)
 {
-    set_boolean_parameter(log_conflicts_, str, CERT_PARAM_LOG_CONFLICTS,
-                          " logging of certification conflicts.");
-}
+    if (key == Certification::PARAM_LOG_CONFLICTS)
+    {
+        set_boolean_parameter(log_conflicts_, value, CERT_PARAM_LOG_CONFLICTS,
+                              "logging of certification conflicts.");
+    }
+    else if (key == Certification::PARAM_OPTIMISTIC_PA)
+    {
+        set_boolean_parameter(optimistic_pa_, value, CERT_PARAM_OPTIMISTIC_PA,
+                              "\"optimistic\" parallel applying.");
+    }
+    else
+    {
+        throw gu::NotFound();
+    }
 
-void
-galera::Certification::set_optimistic_pa(const std::string& str)
-{
-    set_boolean_parameter(optimistic_pa_, str, CERT_PARAM_OPTIMISTIC_PA,
-                          " \"optimistic\" parallel applying.");
+    conf_.set(key, value);
 }
-

galera/src/certification.hpp
@@ -106,8 +106,7 @@ namespace galera
             index_size_ = 0;
         }
 
-        void set_log_conflicts(const std::string& str);
-        void set_optimistic_pa(const std::string& str);
+        void param_set(const std::string& key, const std::string& value);
 
     private:
 
@@ -180,6 +179,7 @@ namespace galera
         };
 
         int           version_;
+        gu::Config&   conf_;
         TrxMap        trx_map_;
         CertIndex     cert_index_;
         CertIndexNG   cert_index_ng_;

galera/src/replicator_smm_params.cpp
@@ -208,23 +208,21 @@ galera::ReplicatorSMM::param_set (const std::string& key,
     if (defaults.map_.find(key) != defaults.map_.end() ||
         key                     == Param::base_host) // is my key?
     {
-        found = true;
         set_param (key, value);
+        found = true;
+        config_.set(key, value);
     }
 
-    if (key == Certification::PARAM_LOG_CONFLICTS)
-    {
-        cert_.set_log_conflicts(value);
-        found = true;
-    }
-    else if (key == Certification::PARAM_OPTIMISTIC_PA)
-    {
-        cert_.set_optimistic_pa(value);
-        found = true;
-    }
     // this key might be for another module
     else if (0 != key.find(common_prefix))
     {
+        try
+        {
+            cert_.param_set (key, value);
+            found = true;
+        }
+        catch (gu::NotFound&) {}
+
         try
         {
             gcs_.param_set (key, value);
@@ -241,8 +239,6 @@ galera::ReplicatorSMM::param_set (const std::string& key,
     }
 
     if (!found) throw gu::NotFound();
-
-    config_.set(key, value);
 }
 
 std::string

galerautils/src/gu_utils.c
@@ -88,6 +88,7 @@ gu_str2bool (const char* str, bool* b)
     case 3:
         if      (!strcasecmp(str, "off")) res = 0;
         else if (!strcasecmp(str, "yes")) res = 1;
+        else if (!strcasecmp(str, "yep")) res = 1;
         break;
     case 4:
         if      (!strcasecmp(str, "true")) res = 1;