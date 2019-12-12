47d112023efa83990cde6d6f137db92f364232ca
Teemu Ollakka <teemu.ollakka@galeracluster.com>
2019-12-12 16:55:20 +0200

    codership/galera#557 Work around assertion from empty vector access.
    Preprocessor option -D_GLIBCXX_ASSERTIONS enables assertions in
    stdlibc++ which fire when accessing empty vector via operator[].
    Worked around the assertion in galera::KeyOS.

--- galera/src/key_os.hpp
+++ galera/src/key_os.hpp
@@ -209,7 +209,9 @@ namespace galera
 
         size_t hash() const
         {
-            return gu_table_hash(&keys_[0], keys_.size());
+            static const unsigned char empty_data[1] = {0};
+            const gu::byte_t* data(keys_.empty() ? empty_data : &keys_[0]);
+            return gu_table_hash(data, keys_.size());
         }
 
         size_t hash_with_flags() const
