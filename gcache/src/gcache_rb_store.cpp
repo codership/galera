9d17c341d98c6c00d51a6372076038eb210483c8
Alexey Yurchenko <alexey.yurchenko@galeracluster.com>
2020-02-18 22:57:07 +0200

    GCache: RB size_used_ and size_free_ are not properly updated on recovery
    Essentially, `size_used_` is estimated from the `first_` and `next_`
    pointers and therefore includes all continuously recovered buffers
    including both ordered and unordered. After that only `size_free_` is
    corrected and only for "released" buffers. However on recovery
    
     1. only ordered buffers matter, so all unordered buffers should be
        mercilessly discarded
     2. remaining ordered buffers are not in use, so they must be "freed"
        and `size_used_` by the end of recovery should be 0
    
    Otherwise non-0 `size_used_` effectively decreases the available ring
    buffer capacity since nobody will call `free()` on those buffers and in
    the case of a lucky (i.e. almost all buffers recovered) ring buffer
    recovery `size_used_` will constitute almost all of the ring buffer size.
    
    codership/galera-bugs#421

--- gcache/src/gcache_rb_store.cpp
+++ gcache/src/gcache_rb_store.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010-2018 Codership Oy <info@codership.com>
+ * Copyright (C) 2010-2020 Codership Oy <info@codership.com>
  */
 
 #include "gcache_rb_store.hpp"
@@ -1025,10 +1025,21 @@ namespace gcache
                 if (gu_likely(bh->size > 0))
                 {
                     total++;
-                    if (gu_unlikely(SEQNO_ILL == bh->seqno_g))
+
+                    if (gu_likely(bh->seqno_g > 0))
                     {
+                        free(bh); // on recovery no buffer is used
+                    }
+                    else
+                    {
+                        /* anything that is not ordered must be discarded */
+                        assert(SEQNO_NONE == bh->seqno_g ||
+                               SEQNO_ILL  == bh->seqno_g);
                         locked++;
+                        empty_buffer(bh);
                         discard(bh);
+                        size_used_ -= bh->size;
+                        // size_free_ is taken care of in discard()
                     }
 
                     bh = BH_next(bh);
@@ -1043,10 +1054,13 @@ namespace gcache
 
             progress.finish();
 
+            /* No buffers on recovery should be in used state */
+            assert(0 == size_used_);
+
             log_info << "GCache DEBUG: RingBuffer::recover(): found "
                      << locked << '/' << total << " locked buffers";
-            log_info << "GCache DEBUG: RingBuffer::recover(): used space: "
-                     << size_used_ << '/' << size_cache_;
+            log_info << "GCache DEBUG: RingBuffer::recover(): free space: "
+                     << size_free_ << '/' << size_cache_;
 
             assert_sizes();
         }
