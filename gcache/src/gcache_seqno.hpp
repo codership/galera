d4f2eaaa3630817670e1497a50447026f67cefa8
Alexey Yurchenko <alexey.yurchenko@galeracluster.com>
2020-06-25 02:03:10 +0300

    Refs codership/galera-bugs#499 Extended the fix toc over all GCC 5 variants and below

--- gcache/src/gcache_seqno.hpp
+++ gcache/src/gcache_seqno.hpp
@@ -15,7 +15,7 @@ namespace gcache
     static seqno_t const SEQNO_NONE =  0;
     static seqno_t const SEQNO_ILL  = -1;
     static seqno_t const SEQNO_MAX
-#if __GNUC__ == 5 && __GNUC_MINOR__ == 4 /* workaround for Xenial GCC bug */
+#if __GNUC__ <= 5  /* workaround for GCC 5 (and below) bug */
                                     __attribute__((unused))
 #endif
                                     = std::numeric_limits<seqno_t>::max();
