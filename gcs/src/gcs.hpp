d6566f8662ea670bc4eab7eb16abde3dedff36d0
Alexey Yurchenko <alexey.yurchenko@galeracluster.com>
2020-08-24 13:07:24 +0300

    Add gmcast segment information to status variables Remove dead stats code
    Refs codership/galera-bugs#537

--- gcs/src/gcs.hpp
+++ gcs/src/gcs.hpp
@@ -420,14 +420,6 @@ typedef struct gcs_act_conf {
                                 *  incoming address, 8-byte cached seqno) */
 } gcs_act_conf_t;
 
-typedef struct gcs_backend_stats {
-    struct stats_t {
-        const char* key;
-        const char* value;
-    }* stats;
-    void* ctx;
-} gcs_backend_stats_t;
-
 struct gcs_stats
 {
     double    send_q_len_avg; //! average send queue length per send call
@@ -446,7 +438,6 @@ struct gcs_stats
     int       send_q_len_min; //! minimum send queue length
     bool      fc_active;      //! flow control is currently active
     bool      fc_requested;   //! flow control is requested by this node
-    gcs_backend_stats_t backend_stats; //! backend stats.
 };
 
 /*! Fills stats struct */
