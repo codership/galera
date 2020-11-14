c17fee9dd01f629b2ee1f9e3788f0e99be57ddc7
Teemu Ollakka <teemu.ollakka@galeracluster.com>
2020-11-14 22:25:34 +0200

    Ensure correct order of linked libraries.

--- cmake/os.cmake
+++ cmake/os.cmake
@@ -1,7 +1,7 @@
 #
 # Copyright (C) 2020 Codership Oy <info@codership.com>
 #
-# OS specific tweaks. 
+# OS specific tweaks and libraries.
 #
 
 # On BSDs packages are usually installed under /usr/local
@@ -9,3 +9,9 @@ if (CMAKE_SYSTEM_NAME MATCHES ".*BSD")
    list(APPEND CMAKE_REQUIRED_INCLUDES "/usr/local/include")
    link_directories(/usr/local/lib)
 endif()
+
+find_library(PTHREAD_LIB pthread)
+find_library(RT_LIB rt)
+set(GALERA_SYSTEM_LIBS ${PTHREAD_LIB} ${RT_LIB})
+
+message(STATUS "Galera system libs: ${GALERA_SYSTEM_LIBS}")
