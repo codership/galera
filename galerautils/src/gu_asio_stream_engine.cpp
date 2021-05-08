09e20cbb1afc2b3a1a059846e10bb303a2002a37
Bernard Spil <Sp1l@users.noreply.github.com>
2021-05-08 11:28:33 +0000

    Fix build with LibreSSL
    LibreSSL does not have all the additions from OpenSSL 1.1.1

--- galerautils/src/gu_asio_stream_engine.cpp
+++ galerautils/src/gu_asio_stream_engine.cpp
@@ -100,7 +100,7 @@ private:
 
 #include <openssl/ssl.h>
 
-#if OPENSSL_VERSION_NUMBER >= 0x1010100fL
+#if OPENSSL_VERSION_NUMBER >= 0x1010100fL && !defined(LIBRESSL_VERSION_NUMBER)
 #define HAVE_READ_EX
 #define HAVE_WRITE_EX
 #endif
