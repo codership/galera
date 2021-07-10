e3e81d2ad789a2689e9ba330e49da228b761aedd
Alexey Yurchenko <alexey.yurchenko@galeracluster.com>
2021-07-10 13:20:53 +0300

    if GCache::seqno_release() is called with seqno greater than the seqno locked for IST, it goes into tight loop until the end of IST. Instead it should just return. Fixed.
    Refs codership/galera-bugs#703

--- gcache/src/GCache_seqno.cpp
+++ gcache/src/GCache_seqno.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009-2020 Codership Oy <info@codership.com>
+ * Copyright (C) 2009-2021 Codership Oy <info@codership.com>
  */
 
 #include "gcache_bh.hpp"
@@ -121,7 +121,18 @@ namespace gcache
 
             gu::Lock lock(mtx);
 
-            assert(seqno >= seqno_released);
+            if (seqno < seqno_released || seqno >= seqno_locked)
+            {
+#ifndef NDEBUG
+                if (params.debug())
+                {
+                    log_info << "GCache::seqno_release(" << seqno
+                             << "): seqno_locked: " << seqno_locked
+                             << ": exiting.";
+                }
+#endif
+                break;
+            }
 
             seqno_t idx(seqno2ptr.upper_bound(seqno_released));
 
