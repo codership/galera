8545d1db3eee8232969e6fefa6fe8f4f88a78ef1
Thomas Klausner <wiz@gatalith.at>
2023-01-13 16:57:19 +0100

    Add NetBSD support following the FreeBSD example.
    In one case, follow Solaris instead.

--- galerautils/src/gu_asio_socket_util.hpp
+++ galerautils/src/gu_asio_socket_util.hpp
@@ -148,7 +148,7 @@ static struct tcp_info get_tcp_info(Socket& socket)
 {
     struct tcp_info tcpi;
     memset(&tcpi, 0, sizeof(tcpi));
-#if defined(__linux__) || defined(__FreeBSD__)
+#if defined(__linux__) || defined(__FreeBSD__) || defined(__NetBSD__)
 #if defined(__linux__)
     static int const level(SOL_TCP);
 #else /* FreeBSD */
@@ -162,7 +162,7 @@ static struct tcp_info get_tcp_info(Socket& socket)
         gu_throw_error(err) << "Failed to read TCP info from socket: "
                             << strerror(err);
     }
-#endif /* __linux__ || __FreeBSD__ */
+#endif /* __linux__ || __FreeBSD__ || __NetBSD__ */
     return tcpi;
 }
 
--- galerautils/src/gu_crc32c_arm64.c
+++ galerautils/src/gu_crc32c_arm64.c
@@ -70,7 +70,7 @@ gu_crc32c_arm64(gu_crc32c_t state, const void* data, size_t len)
 
 #include <sys/auxv.h>
 
-#if defined(__FreeBSD__)
+#if defined(__FreeBSD__) || defined(__NetBSD__)
 /* Imitate getauxval() interface */
 static unsigned long int
 getauxval(unsigned long int const type)
@@ -79,7 +79,7 @@ getauxval(unsigned long int const type)
     if (0 != elf_aux_info(type, &ret, sizeof(ret))) ret = 0;
     return ret;
 }
-#endif /* FreeBSD */
+#endif /* FreeBSD || NetBSD */
 
 #if defined(HWCAP_CRC32)
 #    define GU_AT_HWCAP    AT_HWCAP
