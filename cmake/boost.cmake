5f5001ccf7b1b8c6fc05918bae6cd38c86ad3283
Teemu Ollakka <teemu.ollakka@galeracluster.com>
2023-10-08 09:55:11 +0300

    Define BOOST_BIND_GLOBAL_PLACEHOLDERS=1
    This is to suppress deprecation message about placeholders in
    global namespace:
    
        The practice of declaring the Bind placeholders (_1, _2, ...)
        in the global namespace is deprecated. Please use
        <boost/bind/bind.hpp> + using namespace boost::placeholders,
        or define BOOST_BIND_GLOBAL_PLACEHOLDERS to retain the current
        behavior.

--- cmake/boost.cmake
+++ cmake/boost.cmake
@@ -19,3 +19,17 @@ endif()
 
 # Use nanosecond time precision
 add_definitions(-DBOOST_DATE_TIME_POSIX_TIME_STD_CONFIG=1)
+# This is to suppress deprecation message
+#
+#     The practice of declaring the Bind placeholders (_1, _2, ...)
+#     in the global namespace is deprecated. Please use
+#     <boost/bind/bind.hpp> + using namespace boost::placeholders,
+#     or define BOOST_BIND_GLOBAL_PLACEHOLDERS to retain the current
+#     behavior.
+#
+# Using <boost/bind/bind.hpp> + boost:placeholders worked on all
+# platforms, except on CentOS/RHEL 7 where the boost version is
+# 1.53.0. Consider removing -DBOOST_BIND_GLOBAL_PLACEHOLDERS
+# and using boost::placeholders after CentOS/RHEL builds are
+# not needed anymore.
+add_definitions(-DBOOST_BIND_GLOBAL_PLACEHOLDERS=1)
