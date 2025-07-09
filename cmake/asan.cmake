7d826ace55ed95e3acdd49d5d60d3ac08491e68d
Daniel Black <daniel@mariadb.org>
2025-07-09 14:03:10 +1000

    Add GALERA_WITH_ASAN_SCOPED option
    This mirrors the MariaDB server with
    the compile flag -fsanitize-address-use-after-scope

--- cmake/asan.cmake
+++ cmake/asan.cmake
@@ -6,6 +6,11 @@ if (GALERA_WITH_ASAN)
   set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fsanitize=address")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=address")
   add_definitions(-DGALERA_WITH_ASAN)
+
+  if (GALERA_WITH_ASAN_SCOPED)
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fsanitize-address-use-after-scope")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize-address-use-after-scope")
+  endif()
 endif()
 
 if (GALERA_WITH_UBSAN)
