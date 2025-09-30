codership/galera

commit ddbcc2917dbdb73f9292b1636c01b8c57ac5f262

Refs codership/galera-bugs#938 Commit cut tracking is broken on node leave
Suppose we have a 3-node cluster and node 3 is marked as having the
smallest LAST_APPLIED seqno. Then if node 3 leaves the cluster, leaving
nodes 1 and 2 in primary view, LAST_APPLIED messages from the remaining
nodes no longer will be counted and commit cut won't progress. As a result
certification index won't be purged in time. This happens because GCS
would still be waiting for LAST_APPLIED from node 3 because
group_redo_last_applied() is not called on new primary view.

This requires bumping GCS protocol version for backward compatibility.

gcs/src/gcs_act_proto.hpp
@@ -22,8 +22,10 @@ typedef uint8_t gcs_proto_t;
  * 0 - initial version
  * 1 - support for totally ordered CC events
  * 2 - support for commit cut in state exchange msg
+ * 3 - fix for commit cut tracking issue
+ *     (needs protocol version bump to keep it identical on all nodes)
  */
-#define GCS_PROTO_MAX 2
+#define GCS_PROTO_MAX 3
 
 /*! Internal action fragment data representation */
 typedef struct gcs_act_frag

gcs/src/gcs_group.cpp
@@ -230,8 +230,8 @@ group_redo_last_applied (gcs_group_t* group)
         assert( 0  < group->last_applied_proto_ver ||
                -1 == group->last_applied_proto_ver /* for unit tests */);
 
-        log_debug << "last_last_applied[" << n << "]: "
-                  << node->id << ", " << node->last_applied << ", "
+        log_debug << "last_last_applied[" << group->nodes[n].name << "]: "
+                  << node->id << ", " << node->last_applied << ", count: "
                   << (group_count_last_applied(*group, *node) ? "yes" : "no");
 
         /* NOTE: It is crucial for consistency that last_applied algorithm
@@ -287,7 +287,7 @@ group_redo_last_applied (gcs_group_t* group)
     }
 
     log_debug << "final last_applied on " << group->nodes[group->my_idx].name
-              << "): " << group->last_applied;
+              << ": " << group->last_applied;
 }
 
 static void
@@ -436,8 +436,10 @@ group_post_state_exchange (gcs_group_t* group)
             group->prim_uuid  = group->state_uuid;
             group->state_uuid = GU_UUID_NIL;
 
-            if (quorum->gcs_proto_ver >= 2) // see below for older version
+            if (quorum->gcs_proto_ver == 2) // see below for other versions
             {
+                /* version 2 was a mistake, but we can't eliminate this code
+                 * path for the sake of backward compatibility */
                 assert(quorum->last_applied >= 0);
                 group->last_applied = quorum->last_applied;
             }
@@ -465,11 +467,10 @@ group_post_state_exchange (gcs_group_t* group)
         GROUP_UPDATE_PROTO_VER(appl);
 #undef GROUP_UPDATE_PROTO_VER
 
-        if (quorum->gcs_proto_ver < 2) // see above for newer version
+        if (quorum->gcs_proto_ver != 2) // see above for version 2
         {
             group_redo_last_applied(group);
         }
-        // votes will be recounted on CC action creation
     }
     else {
         // non-primary configuration
@@ -811,6 +812,11 @@ gcs_group_handle_last_msg (gcs_group_t* group, const gcs_recv_msg_t* msg)
 
     gcs_node_set_last_applied (&group->nodes[msg->sender_idx], gtid.seqno());
     assert(group->nodes[msg->sender_idx].last_applied >= 0);
+
+    log_debug << "Got last applied " << gtid.seqno() << " from "
+              << msg->sender_idx << " (" << group->nodes[msg->sender_idx].name
+              << "). Last node: " << group->last_node << " ("
+              << group->nodes[group->last_node].name << ")";
 
     if (msg->sender_idx == group->last_node   &&
         gtid.seqno()    >  group->last_applied) {

gcs/src/gcs.cpp
@@ -2230,6 +2230,7 @@ gcs_set_last_applied (gcs_conn_t* conn, const gu::GTID& gtid)
     }
     else
     {
+        log_debug << "Sending last applied seqno: " << gtid.seqno();
         ret = gcs_core_set_last_applied(conn->core, gtid);
         gcs_sm_leave(conn->sm);
         if (ret < 0)

commit 0f34ba2b6053c8954b17ac186004f9600acbb0c6

MGL-75 Old last_applied value is inherited into new history
If a node rejoins a new cluster component with a new history (for example
a new primary component is bootstrapped during network partition) it still
carries old and likely very high last_applied counter with it and

 - will be unable to process new LAST messages from itself as long as their
   seqno is below that last_applied value preserved from old history.
 - will "infect" the rest of the cluster with that last_applied value if it
   for any reason becomes the only member of the primary configuration.

It is a regression and was introduced in commit ddbcc2917d.

Revert that mimstake (requires GCS protocol bump) and implement a way to
"desinfect" the cluster.

gcs/src/gcs_act_proto.hpp
@@ -28,8 +28,10 @@ typedef uint8_t gcs_proto_t;
  *     (must keep it identical on all nodes)
  * 5 - fix for commit cut tracking for just SYNCED nodes
  *     (must keep it identical on all nodes)
+ * 6 - fix for commit cut "infection" from a node with different history
+ *     (must keep it identical on all nodes)
  */
-#define GCS_PROTO_MAX 5
+#define GCS_PROTO_MAX 6
 
 /*! Internal action fragment data representation */
 typedef struct gcs_act_frag

gcs/src/gcs_group.cpp
@@ -486,6 +486,15 @@ group_post_state_exchange (gcs_group_t* group)
                 assert(quorum->last_applied >= 0);
                 group->last_applied = quorum->last_applied;
             }
+
+            if (quorum->gcs_proto_ver >= 6) {
+                /* removing
+                   group->last_applied = quorum->last_applied;
+                 * above was a mistake as it allowed last_applied
+                 * from the old history survive into the new one.
+                 * Reinstating with new protocol version. */
+                group->last_applied = quorum->last_applied;
+            }
         }
         else {
             // no state exchange happend, processing old state messages

gcs/src/gcs_node.cpp
@@ -44,6 +44,18 @@ gcs_node_init (gcs_node_t* const node,
     node->stateless      = stateless;
 }
 
+/*! Reset certain node properties after it joins a different cluster.
+ *  Setting to 0 as it is what it is initialized to in gcs_node_init(). */
+static void
+node_reset(gcs_node_t* node)
+{
+    node->bootstrap = false;
+    node->status    = GCS_NODE_STATE_NON_PRIM;
+    node->last_applied = 0;
+    node->vote_seqno= GCS_NO_VOTE_SEQNO;
+    node->vote_res  = 0;
+}
+
 /*! Move data from one node object to another */
 void
 gcs_node_move (gcs_node_t* dst, gcs_node_t* src)
@@ -213,9 +225,12 @@ gcs_node_update_status (gcs_node_t* node, const gcs_state_quorum_t* quorum)
         }
         else {
             // node joins completely different group, clear all status
-            if (node->status > GCS_NODE_STATE_PRIM) {
+            if (node->status > GCS_NODE_STATE_PRIM || node->last_applied > 0) {
                 gu_info ("'%s' has a different history, demoted %s->PRIMARY",
                          node->name, gcs_node_state_to_str(node->status));
+            }
+            if (quorum->gcs_proto_ver >= 6) {
+                node_reset(node);
             }
             node->status = GCS_NODE_STATE_PRIM;
         }

gcs/src/gcs_state_msg.cpp
@@ -995,6 +995,15 @@ gcs_state_msg_get_quorum (const gcs_state_msg_t* states[],
         quorum->vote_policy = rep->vote_policy;
     }
 
+    if (quorum->gcs_proto_ver >= 6)
+    {
+        if (quorum->last_applied > quorum->act_id)
+        {
+            // commit cut is infected from pre v6 versions, reset it
+            quorum->last_applied = 0;
+        }
+    }
+
     if (quorum->version < 2) {;} // for future generations
 
     if (quorum->version < 1) {