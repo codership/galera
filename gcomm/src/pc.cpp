4dc44079458bd9c9fdc908c9764664821d8b0410
Pekka Lampio <pekka.lampio@galeracluster.com>
2026-01-21 16:54:45 +0200

    MENT-2489 MariaDB crashes with signal-11 when wsrep_applier_priority /           gcomm.thread_prio is set to a non-zero value
    Modify error handling of thread priority setting in gcomm. Now a
    failure prints a log message instead of throwing an exception.

--- gcomm/src/pc.cpp
+++ gcomm/src/pc.cpp
@@ -157,6 +157,7 @@ void gcomm::PC::connect(bool start_prim)
             pstack_.pop_proto(pc_);
             pstack_.pop_proto(evs_);
             pstack_.pop_proto(gmcast_);
+            closed_ = true;
             gu_throw_error(ETIMEDOUT) << "failed to reach primary view";
         }
     }
@@ -173,6 +174,10 @@ void gcomm::PC::connect(const gu::URI& uri)
 
 void gcomm::PC::close(bool force)
 {
+    if (closed_ == true)
+    {
+        return;
+    }
     if (force == true)
     {
         log_info << "Forced PC close";
@@ -180,8 +185,6 @@ void gcomm::PC::close(bool force)
         // Don't bother closing PC and EVS at this point. Currently
         // there is no way of knowing why forced close was issued,
         // so graceful close of PC and/or EVS may not be safe.
-        // pc_->close();
-        // evs_->close();
     }
     else
     {
