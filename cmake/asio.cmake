7f86cfd3278584e8019d18309cf8587b80c0673b
Teemu Ollakka <teemu.ollakka@mariadb.com>
2026-06-09 09:19:48 +0300

    MDEV-36926 Print the selected Asio version at configure time
    Add REPORT_ASIO_VERSION() to cmake/asio.cmake, which locates the
    asio/version.hpp of the Asio actually picked (custom path, system, or
    bundled), decodes ASIO_VERSION and prints "Using Asio version
    <major>.<minor>.<patch>" so the configure output shows which Asio the
    build resolved to.

--- cmake/asio.cmake
+++ cmake/asio.cmake
@@ -37,6 +37,38 @@ int main()
   set(CMAKE_REQUIRED_INCLUDES ${SAVED_CMAKE_REQUIRED_INCLUDES})
 endmacro()
 
+# Print the major.minor.patch of the Asio that will actually be used.
+# HEADER_DIR is a directory containing "asio/version.hpp"; when empty the
+# default system include paths are searched instead.
+function(REPORT_ASIO_VERSION HEADER_DIR)
+  if(HEADER_DIR)
+    set(version_hpp "${HEADER_DIR}/asio/version.hpp")
+  else()
+    find_file(GALERA_ASIO_VERSION_HPP asio/version.hpp)
+    set(version_hpp "${GALERA_ASIO_VERSION_HPP}")
+    unset(GALERA_ASIO_VERSION_HPP CACHE)
+  endif()
+
+  if(NOT version_hpp OR NOT EXISTS "${version_hpp}")
+    message(STATUS "Asio version: unable to locate asio/version.hpp")
+    return()
+  endif()
+
+  file(STRINGS "${version_hpp}" version_line
+    REGEX "^#define[ \t]+ASIO_VERSION[ \t]+[0-9]+")
+  string(REGEX MATCH "[0-9]+" version "${version_line}")
+  if(NOT version)
+    message(STATUS "Asio version: unable to parse ASIO_VERSION from ${version_hpp}")
+    return()
+  endif()
+
+  # ASIO_VERSION == major * 100000 + minor * 100 + sub_minor
+  math(EXPR major "${version} / 100000")
+  math(EXPR minor "(${version} / 100) % 1000")
+  math(EXPR sub_minor "${version} % 100")
+  message(STATUS "Using Asio version ${major}.${minor}.${sub_minor} (${version_hpp})")
+endfunction()
+
 if(GALERA_CUSTOM_ASIO_PATH)
   message(STATUS "Using custom Asio path: ${GALERA_CUSTOM_ASIO_PATH}")
   include_directories(SYSTEM ${GALERA_CUSTOM_ASIO_PATH})
@@ -49,6 +81,7 @@ if (HAVE_SYSTEM_ASIO_HPP)
   CHECK_ASIO_VERSION()
   if (ASIO_VERSION_OK)
     add_definitions(-DHAVE_ASIO_HPP)
+    REPORT_ASIO_VERSION("${GALERA_CUSTOM_ASIO_PATH}")
   else()
     unset(HAVE_SYSTEM_ASIO_HPP CACHE)
     unset(ASIO_VERSION_OK CACHE)
@@ -57,6 +90,7 @@ endif()
 
 if(NOT ASIO_VERSION_OK)
   message(STATUS "Using bundled asio")
+  REPORT_ASIO_VERSION("${PROJECT_SOURCE_DIR}/asio")
   include_directories(SYSTEM ${PROJECT_SOURCE_DIR}/asio)
 endif()
 
